634baeb5b3e2e6a7ddfe5ec8abb626216ff8402a
robertdkirkby <robertdkirkby@gmail.com>
2015-06-21 19:11:18 +1200

    Added SubCodes Kron and UnKron PolicyIndexes

---
+++ SubCodes/UnKronPolicyIndexes/UnKronPolicyIndexes_Case2.m
@@ -0,0 +1,39 @@
+function Policy=UnKronPolicyIndexes_Case2(Policy, n_d, n_a, n_z,vfoptions)
+
+%PolicyIndexesKron=zeros(N_a,N_z); %first dim indexes the optimal choice for d and aprime rest of dimensions a,z
+%PolicyIndexesIntermediate (l_d,N_a,N_z)
+%PolicyIndexes (l_d,n_a,n_z);
+
+N_a=prod(n_a);
+N_z=prod(n_z);
+
+l_d=length(n_d);
+
+if vfoptions.parallel~=2
+    PolicyTemp=zeros(l_d,N_a,N_z);
+    for i=1:N_a
+        for j=1:N_z
+            optdindexKron=Policy(i,j);
+            optD=ind2sub_homemade(n_d',optdindexKron);
+            PolicyTemp(:,i,j)=[optD'];
+        end
+    end
+    Policy=reshape(PolicyTemp,[l_d,n_a,n_z]);
+else
+    PolicyTemp=zeros(l_d,N_a,N_z,'gpuArray');
+    
+    PolicyTemp(1,:,:)=shiftdim(rem(Policy-1,n_d(1))+1,-1);
+    if l_d>1
+        if l_d>2
+            for ii=2:l_d-1
+                PolicyTemp(ii,:,:)=shiftdim(rem(ceil(Policy/prod(n_d(1:ii-1)))-1,n_d(ii))+1,-1);
+            end
+        end
+        PolicyTemp(l_d,:,:)=shiftdim(ceil(Policy/prod(n_d(1:l_d-1))),-1);
+    end
+    
+    Policy=reshape(PolicyTemp,[l_d,n_a,n_z]);
+end
+    
+
+end
