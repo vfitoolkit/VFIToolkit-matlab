282108a6d0fb9890a2906fe533e9d45fb155e979
robertdkirkby <robertdkirkby@gmail.com>
2016-02-03 20:21:04 +1300

    Give user a warning if a parameter is not found

--- SubCodes/ParameterStructure/CreateVectorFromParams.m
+++ SubCodes/ParameterStructure/CreateVectorFromParams.m
@@ -11,10 +11,15 @@ nFields=length(FullParamNames);
 
 VectorOfParamValues=nan(nCalibParams,1);
 for iCalibParam = 1:nCalibParams
+    found=0;
     for iField=1:nFields
         if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
             VectorOfParamValues(iCalibParam)=Parameters.(FullParamNames{iField});
+            found=1;
         end
+    end
+    if found==0 % Have added this check so that user can see if they are missing a parameter
+        fprintf(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
     end
 end
 
