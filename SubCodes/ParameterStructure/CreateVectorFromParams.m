99d62430f4011a6ccfb9cae822ef99a179e15744
robertdkirkby <robertdkirkby@gmail.com>
2016-08-03 16:07:18 +1200

    CreateVectorFromParams now allows extra arguments as part of parameters that depend on age or type

--- SubCodes/ParameterStructure/CreateVectorFromParams.m
+++ SubCodes/ParameterStructure/CreateVectorFromParams.m
@@ -1,25 +1,72 @@
-function VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames)
+function VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames,index1,index2)
 
 % CreateVectorFromParams looks in structure called 'Parameters' and 
 % then creates a vector containing the values of it's fields that
 % correspond to those field names in ParamNames (and in the order
 % given by CalibParamNames)
 
+% Some parameters are stored in the Parameters structure as vectors or
+% matrices (eg., because the parameter values depends on age, or on agent
+% type). In these cases 'index1' (and 'index2') can be used to specify which
+% is the relevant element.
+
 nCalibParams=length(ParamNames);
 FullParamNames=fieldnames(Parameters);
 nFields=length(FullParamNames);
 
 VectorOfParamValues=nan(nCalibParams,1);
-for iCalibParam = 1:nCalibParams
-    found=0;
-    for iField=1:nFields
-        if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
-            VectorOfParamValues(iCalibParam)=Parameters.(FullParamNames{iField});
-            found=1;
+if nargin==2
+    for iCalibParam = 1:nCalibParams
+        found=0;
+        for iField=1:nFields
+            if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
+                VectorOfParamValues(iCalibParam)=Parameters.(FullParamNames{iField});
+                found=1;
+            end
+        end
+        if found==0 % Have added this check so that user can see if they are missing a parameter
+            fprintf(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
         end
     end
-    if found==0 % Have added this check so that user can see if they are missing a parameter
-        fprintf(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
+elseif nargin==3
+    for iCalibParam = 1:nCalibParams
+        found=0;
+        for iField=1:nFields
+            if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
+                temp=Parameters.(FullParamNames{iField});
+                if length(temp)>1 % Some parameters will depend on the index, some will not.
+                    VectorOfParamValues(iCalibParam)=temp(index1); 
+                else
+                    VectorOfParamValues(iCalibParam)=temp;
+                end
+                found=1;
+            end
+        end
+        if found==0 % Have added this check so that user can see if they are missing a parameter
+            fprintf(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
+        end
+    end
+elseif nargin==4
+    for iCalibParam = 1:nCalibParams
+        found=0;
+        for iField=1:nFields
+            if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
+                temp=Parameters.(FullParamNames{iField});
+                if numel(temp)>length(temp) % Some parameters will depend on both index1 and index2
+                    VectorOfParamValues(iCalibParam)=temp(index1,index2);
+                elseif size(temp,1)==length(temp) % Some parameters will depend only on index1.
+                    VectorOfParamValues(iCalibParam)=temp(index1); 
+                elseif size(temp,2)==length(temp) % Some parameters will depend only on index2.
+                    VectorOfParamValues(iCalibParam)=temp(1,index2); 
+                else
+                    VectorOfParamValues(iCalibParam)=temp;
+                end
+                found=1;
+            end
+        end
+        if found==0 % Have added this check so that user can see if they are missing a parameter
+            fprintf(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
+        end
     end
 end
 
