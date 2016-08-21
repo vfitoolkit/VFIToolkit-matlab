323736e80d66bd174da8a8c5d78b1f6f99b18540
robertdkirkby <robertdkirkby@gmail.com>
2016-08-21 18:42:48 +1200

    Implemented fixed types for finite horizon value functions

--- ValueFnIter/FHorz_PType/ValueFnIter_Case1_FHorz_PType.m
+++ ValueFnIter/FHorz_PType/ValueFnIter_Case1_FHorz_PType.m
@@ -1,14 +1,77 @@
-function [V, PolicyIndexes]=ValueFnIter_Case1_FHorz_PType(n_d,n_a,n_z,n_i,N_j, pi_z, beta_j, FmatrixFn_ij)
+function [V, Policy]=ValueFnIter_Case1_FHorz_PType(n_d,n_a,n_z,N_i, N_j,d_grid, a_grid, z_grid, pi_z, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions)
+%[V, PolicyIndexes]=ValueFnIter_Case1_FHorz_PType(n_d,n_a,n_z,n_i,N_j, pi_z, beta_j, FmatrixFn_ij)
 
-N_d=prod(n_d);
-N_a=prod(n_a);
-N_z=prod(n_z);
-N_i=prod(n_i);
+% N_d=prod(n_d);
+% N_a=prod(n_a);
+% N_z=prod(n_z);
+% N_i=prod(n_i);
 
-[VKron, PolicyIndexesKron]=ValueFnIter_Case1_FHorz_PType_raw(N_d,N_a,N_z,N_i,N_j, pi_z, beta_j, FmatrixFn_ij);
+for ii=1:N_i
+    try
+        if vfoptions.verbose==1
+            sprintf('Fixed type: %i of %i',ii, N_i)
+        end
+    end
+    
+    % Go through everything which might be dependent on fixed type (PType)
+    % [THIS could be better coded]
+    d_grid_temp=d_grid;
+    if isa(d_grid,'struct')
+        names=fieldnames(d_grid);
+        d_grid_temp=d_grid.(names{ii});
+    end
+    a_grid_temp=a_grid;
+    if isa(a_grid,'struct')
+        names=fieldnames(a_grid);
+        a_grid_temp=a_grid.(names{ii});        
+    end
+    z_grid_temp=z_grid;
+    if isa(z_grid,'struct')
+        names=fieldnames(z_grid);
+        z_grid_temp=z_grid.(names{ii});
+    end
+    pi_z_temp=pi_z;
+        if isa(pi_z,'struct')
+        names=fieldnames(pi_z);
+        pi_z_temp=pi_z.(names{ii});
+    end
+    ReturnFn_temp=ReturnFn;
+    if isa(ReturnFn,'struct')
+        names=fieldnames(ReturnFn);
+        ReturnFn_temp=ReturnFn.(names{ii});
+    end
+    Parameters_temp=Parameters;
+    FullParamNames=fieldnames(Parameters);
+    nFields=length(FullParamNames);
+    for kField=1:nFields
+        if isa(Parameters.(FullParamNames{kField}), 'struct')
+            names=fieldnames(Parameters.(FullParamNames{kField}));
+            Parameters_temp.(FullParamNames{kField})=Parameters.(FullParamNames{kField}).(names{ii});
+        end
+    end
+    DiscountFactorParamNames_temp=DiscountFactorParamNames;
+    if isa(DiscountFactorParamNames,'struct')
+        names=fieldnames(DiscountFactorParamNames);
+        DiscountFactorParamNames_temp=DiscountFactorParamNames.(names{ii});
+    end
+    ReturnFnParamNames_temp=ReturnFnParamNames;
+    if isa(ReturnFnParamNames,'struct')
+        names=fieldnames(ReturnFnParamNames);
+        ReturnFnParamNames_temp=ReturnFnParamNames.(names{ii});
+    end
+    
+    try vfoptions % check whether vfoptions was inputted
+        [V_ii, Policy_ii]=ValueFnIter_Case1_FHorz(n_d,n_a,n_z,N_j,d_grid_temp, a_grid_temp, z_grid_temp, pi_z_temp, ReturnFn_temp, Parameters_temp, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+    catch
+        [V_ii, Policy_ii]=ValueFnIter_Case1_FHorz(n_d,n_a,n_z,N_j,d_grid_temp, a_grid_temp, z_grid_temp, pi_z_temp, ReturnFn_temp, Parameters_temp, DiscountFactorParamNames_temp, ReturnFnParamNames_temp);
+    end
+    
+    ftstring=['ft',num2str(ii)];
+    
+    V.(ftstring)=V_ii;
+    Policy.(ftstring)=Policy_ii;    
 
-%Transforming Value Fn and Optimal Policy Indexes matrices back out of Kronecker Form
-V=reshape(VKron,[n_a,n_z,N_i,N_j]);
-PolicyIndexes=UnKronPolicyIndexes_Case1_FHorz_PType(PolicyIndexesKron, n_d, n_a, n_z, N_i,N_j);
+end
+
 
 end
