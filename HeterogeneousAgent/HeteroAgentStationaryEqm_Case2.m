b05c94423c42d3d4144977761a6e1a22b2bf2ca9
robertdkirkby <robertdkirkby@gmail.com>
2017-01-10 11:50:54 +1300

    trivial correction

--- HeterogeneousAgent/HeteroAgentStationaryEqm_Case2.m
+++ HeterogeneousAgent/HeteroAgentStationaryEqm_Case2.m
@@ -127,11 +127,13 @@ disp(sprintf('If this number is not zero there is an problem with Phi_aprimeKron
 %%
 V0Kron=reshape(V0,[N_a,N_s]);
 
+
 if N_p~=0
     [p_eqm,p_eqm_index,MarketClearance]=HeteroAgentStationaryEqm_Case2_pgrid(V0Kron, n_d, n_a, n_s, n_p, pi_s, d_grid, a_grid, s_grid, Phi_aprimeKron, Case2_Type, ReturnFn, SSvaluesFn, MarketPriceEqns, Parameters, DiscountFactorParamNames, ReturnFnParamNames, PhiaprimeParamNames, SSvalueParamNames, MarketPriceParamNames, PriceParamNames,heteroagentoptions, simoptions, vfoptions);
+    return
 end
 
-%% 
+%%  Otherwise, use fminsearch to find the general equilibrium
 % I SHOULD IMPLEMENT A BETTER V0Kron HERE
 MarketClearanceFn=@(p) HeteroAgentStationaryEqm_Case2_subfn(p, V0Kron, n_d, n_a, n_s, n_p, pi_s, d_grid, a_grid, s_grid, Phi_aprimeKron, Case2_Type, ReturnFn, SSvaluesFn, MarketPriceEqns, Parameters, DiscountFactorParamNames, ReturnFnParamNames, PhiaprimeParamNames, SSvalueParamNames, MarketPriceParamNames, PriceParamNames,heteroagentoptions, simoptions, vfoptions)
 
@@ -151,74 +153,5 @@ end
 
 p_eqm_index=nan; % If not using p_grid then this is irrelevant/useless
 
-%% This is some legacy code, can prob be deleted.
-%MarketClearanceKron=ones(N_p,l_p);
-%V0Kron=reshape(V0,[N_a,N_s]);
-% 
-% disp('Running through possible prices for general eqm')
-% 
-% for p_c=1:N_p
-%     V0Kron(~isfinite(V0Kron))=0; %Since we loop through with V0Kron from previous p_c this is necessary to avoid contamination by -Inf's
-%     %Calculate the price vector associated with p_c
-%     p_index=ind2sub_homemade(n_p,p_c);
-%     p=cell(l_p,1);
-%     for i=1:l_p
-%         if i==1
-%             p{i}=p_grid(p_index(1));
-%         else
-%             p{i}=p_grid(sum(n_p(1:i-1))+p_index(i));
-%         end
-%     end
-%     %p_c
-% 
-%     %tic;
-%     
-%     if vfoptions.returnmatrix==1
-%         ReturnMatrix=ReturnFnHeteroAgent(p{:});
-%     else
-%         ReturnFn=@(d,a,s) ReturnFnHeteroAgent(d,a,s,p{:});
-%         ReturnMatrix=CreateReturnFnMatrix_Case2_Disc(ReturnFn, n_d, n_a, n_s, d_grid, a_grid, s_grid, vfoptions.parallel);
-%     end
-%     
-%     if vfoptions.parallel==0
-%         [VKron, PolicyIndexesKron]=ValueFnIter_Case2_raw(V0Kron, n_d,n_a,n_s, pi_s, beta, ReturnMatrix,Phi_aprimeKron,Case2_Type,vfoptions.howards,vfoptions.verbose); 
-%     elseif vfoptions.parallel==1
-%         [VKron, PolicyIndexesKron]=ValueFnIter_Case2_Par1_raw(V0Kron, n_d,n_a,n_s, pi_s, beta, ReturnMatrix,Phi_aprimeKron,Case2_Type,vfoptions.howards,vfoptions.verbose);
-%     end
-%     
-% %    [V0Kron,PolicyIndexesKron]=ValueFnIter_Case2_raw(Tolerance, V0Kron, N_d, N_a, N_s, pi_s, Phi_aprimeKron, Case2_Type, beta, FmatrixKron, Howards);
-%     %Step 2: Calculate the Steady-state distn (given this price)
-%     %and use it to assess market clearance
-%     StationaryDistKron=StationaryDist_Case2_Simulation_raw(PolicyIndexesKron,Phi_aprimeKron,Case2_Type,N_d,N_a,N_s,pi_s, simoptions);
-%     if simoptions.iterate==1
-%         StationaryDistKron=StationaryDist_Case2_Iteration_raw(StationaryDistKron, PolicyIndexesKron,Phi_aprimeKron,Case2_Type,N_d,N_a,N_s,pi_s, simoptions);
-%     end
-%     SSvalues_AggVars=SSvalues_AggVars_Case2_raw(StationaryDistKron, PolicyIndexesKron, SSvaluesFn, n_d, n_a, n_s, d_grid, a_grid, s_grid, pi_s,p);
-%     MarketClearanceKron(p_c,:)=MarketClearance_Case2(SSvalues_AggVars,p_c,n_p,p_grid, MarketPriceEqns, MarketPriceParams);
-%     
-%     %toc
-% end
-% 
-% 
-% if MultiMarketCriterion==1 %the measure of market clearance is to take the sum of squares of clearance in each market 
-%     [trash,p_eqm_indexKron]=min(sum(MarketClearanceKron.^2,2)); 
-% end
-% 
-% %[trash,p_eqm_indexKron]=min(MarketClearanceKron); 
-% %p_eqm_index=zeros(num_p_vars,1);
-% p_eqm_index=ind2sub_homemade(n_p,p_eqm_indexKron);
-% MarketClearance=reshape(MarketClearanceKron,[n_p,1]);
-% 
-% 
-% %Calculate the price associated with p_eqm_index
-% l_p=length(n_p);
-% p_eqm=zeros(l_p,1);
-% for i=1:l_p
-%     if i==1
-%         p_eqm(i)=p_grid(p_eqm_index(1));
-%     else
-%         p_eqm(i)=p_grid(sum(n_p(1:i-1))+p_eqm_index(i));
-%     end
-% end
 
 end
