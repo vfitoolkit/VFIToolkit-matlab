555d71c40bb37b6ba546c560c4166d507716e512
robertdkirkby <robertdkirkby@gmail.com>
2017-04-24 11:11:25 +1200

    MeanMedianStdDev updated to new approach to SteadyStateStatistics

--- SteadyStateStatistics/SSvalues_MeanMedianStdDev_Case1.m
+++ SteadyStateStatistics/SSvalues_MeanMedianStdDev_Case1.m
@@ -1,4 +1,5 @@
-function SSvalues_MeanMedianStdDev=SSvalues_MeanMedianStdDev_Case1(StationaryDist, PolicyIndexes, SSvaluesFn, Parameters, SSvalueParamNames, n_d, n_a, n_z, d_grid, a_grid, z_grid, Parallel)
+function SSvalues_MeanMedianStdDev=SSvalues_MeanMedianStdDev_Case1(StationaryDist, PolicyIndexes, SSvaluesFn, Parameters,SSvalueParamNames, n_d, n_a, n_z, d_grid, a_grid, z_grid, Parallel)
+% Evaluates the mean value (weighted sum/integral), median value, and standard deviation for each element of SSvaluesFn
 
 if n_d(1)==0
     l_d=0;
@@ -11,12 +12,11 @@ l_z=length(n_z);
 N_a=prod(n_a);
 N_z=prod(n_z);
 
-StationaryDistVec=reshape(StationaryDist,[N_a*N_z,1]);
-
 if Parallel==2
+    SSvalues_MeanMedianStdDev=zeros(length(SSvaluesFn),3,'gpuArray'); % 3 columns: Mean, Median, and Standard Deviation
     
-    SSvalues_MeanMedianStdDev=zeros(length(SSvaluesFn),3, 'gpuArray'); % 3 columns: Mean, Median, and Standard Deviation
-        
+    StationaryDistVec=reshape(StationaryDist,[N_a*N_z,1]);
+    
     PolicyValues=PolicyInd2Val_Case1(PolicyIndexes,n_d,n_a,n_z,d_grid,a_grid, Parallel);
     permuteindexes=[1+(1:1:(l_a+l_z)),1];
     PolicyValuesPermute=permute(PolicyValues,permuteindexes); %[n_a,n_s,l_d+l_a]
@@ -28,39 +28,32 @@ if Parallel==2
         else
             SSvalueParamsVec=CreateVectorFromParams(Parameters,SSvalueParamNames(i).Names);
         end
-        
         Values=ValuesOnSSGrid_Case1(SSvaluesFn{i}, SSvalueParamsVec,PolicyValuesPermute,n_d,n_a,n_z,a_grid,z_grid,Parallel);
         Values=reshape(Values,[N_a*N_z,1]);
-        
-        %Mean
+        % Mean
         SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
-        
-        %Median
+        % Median
         [SortedValues,SortedValues_index] = sort(Values);
-        SortedSteadyStateDistVec=StationaryDistVec(SortedValues_index);
-        
-        SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedSteadyStateDistVec)>0.5));
-        
-        %Standard Deviation
+        SortedStationaryDistVec=StationaryDistVec(SortedValues_index);
+        SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedStationaryDistVec)>0.5));
+        % Standard Deviation
         SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
     end
-    
 else
     SSvalues_MeanMedianStdDev=zeros(length(SSvaluesFn),3); % 3 columns: Mean, Median, and Standard Deviation
-    d_val=zeros(l_d,1);
-    aprime_val=zeros(l_a,1);
-    a_val=zeros(l_a,1);
-    s_val=zeros(l_z,1);
-    
-    PolicyIndexesKron=reshape(PolicyIndexes,[l_d+l_a,N_a,N_z]);    
+    d_val=zeros(1,l_d);
+    aprime_val=zeros(1,l_a);
+    a_val=zeros(1,l_a);
+    z_val=zeros(1,l_z);
+    StationaryDistVec=reshape(StationaryDist,[N_a*N_z,1]);
     
     for i=1:length(SSvaluesFn)
         % Includes check for cases in which no parameters are actually required
-        if isempty(SSvalueParamNames) % check for 'SSvalueParamNames={}'
+        if isempty(SSvalueParamNames(i).Names) % check for 'SSvalueParamNames={}'
+            Values=zeros(N_a,N_z);
             if l_d==0
-                Values=zeros(N_a,N_z);
                 for j1=1:N_a
-                    a_ind=ind2sub_homemade([n_a],j1);
+                    a_ind=ind2sub_homemade_gpu([n_a],j1);
                     for jj1=1:l_a
                         if jj1==1
                             a_val(jj1)=a_grid(a_ind(jj1));
@@ -69,15 +62,16 @@ else
                         end
                     end
                     for j2=1:N_z
-                        s_ind=ind2sub_homemade([n_z],j2);
+                        z_ind=ind2sub_homemade_gpu([n_z],j2);
                         for jj2=1:l_z
                             if jj2==1
-                                s_val(jj2)=z_grid(s_ind(jj2));
+                                z_val(jj2)=z_grid(z_ind(jj2));
                             else
-                                s_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                                z_val(jj2)=z_grid(z_ind(jj2)+sum(n_z(1:jj2-1)));
                             end
                         end
-                        aprime_ind=PolicyIndexesKron(l_d+1:l_d+l_a,j1,j2);
+                        [aprime_ind]=PolicyIndexes(:,j1,j2);
+                        
                         for kk2=1:l_a
                             if kk2==1
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2));
@@ -85,27 +79,13 @@ else
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2)+sum(n_a(1:kk2-1)));
                             end
                         end
-                        Values(j1,j2)=SSvaluesFn{i}(aprime_val,a_val,s_val);
+                        Values(j1,j2)=SSvaluesFn{i}(aprime_val,a_val,z_val);
+                        
                     end
                 end
-                
-                Values=reshape(Values,[N_a*N_z,1]);
-                
-                %Mean
-                SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
-                
-                %Median
-                [SortedValues,SortedValues_index] = sort(Values);
-                SortedSteadyStateDistVec=StationaryDistVec(SortedValues_index);
-                
-                SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedSteadyStateDistVec)>0.5));
-                
-                %Standard Deviation
-                SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
             else
-                Values=zeros(N_a,N_z);
                 for j1=1:N_a
-                    a_ind=ind2sub_homemade([n_a],j1);
+                    a_ind=ind2sub_homemade_gpu([n_a],j1);
                     for jj1=1:l_a
                         if jj1==1
                             a_val(jj1)=a_grid(a_ind(jj1));
@@ -114,23 +94,23 @@ else
                         end
                     end
                     for j2=1:N_z
-                        s_ind=ind2sub_homemade([n_z],j2);
+                        z_ind=ind2sub_homemade_gpu([n_z],j2);
                         for jj2=1:l_z
                             if jj2==1
-                                s_val(jj2)=z_grid(s_ind(jj2));
+                                z_val(jj2)=z_grid(z_ind(jj2));
                             else
-                                s_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                                z_val(jj2)=z_grid(z_ind(jj2)+sum(n_z(1:jj2-1)));
                             end
                         end
-                        d_ind=PolicyIndexesKron(1:l_d,j1,j2);
-                        for kk=1:l_d
-                            if kk==1
-                                d_val(kk)=d_grid(d_ind(kk));
+                        d_ind=PolicyIndexes(1:l_d,j1,j2);
+                        aprime_ind=PolicyIndexes(l_d+1:l_d+l_a,j1,j2);
+                        for kk1=1:l_d
+                            if kk1==1
+                                d_val(kk1)=d_grid(d_ind(kk1));
                             else
-                                d_val(kk)=d_grid(d_ind(kk)+sum(n_d(1:kk-1)));
+                                d_val(kk1)=d_grid(d_ind(kk1)+sum(n_d(1:kk1-1)));
                             end
                         end
-                        aprime_ind=PolicyIndexesKron(l_d+1:l_d+l_a,j1,j2);
                         for kk2=1:l_a
                             if kk2==1
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2));
@@ -138,30 +118,27 @@ else
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2)+sum(n_a(1:kk2-1)));
                             end
                         end
-                        Values(j1,j2)=SSvaluesFn{i}(d_val,aprime_val,a_val,s_val);
+
+                        Values(j1,j2)=SSvaluesFn{i}(d_val,aprime_val,a_val,z_val);
                     end
                 end
-                
-                Values=reshape(Values,[N_a*N_z,1]);
-                
-                %Mean
-                SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
-                
-                %Median
-                [SortedValues,SortedValues_index] = sort(Values);
-                SortedSteadyStateDistVec=StationaryDistVec(SortedValues_index);
-                
-                SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedSteadyStateDistVec)>0.5));
-                
-                %Standard Deviation
-                SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
             end
+            Values=reshape(Values,[N_a*N_z,1]);
+            
+            % Mean
+            SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
+            % Median
+            [SortedValues,SortedValues_index] = sort(Values);
+            SortedStationaryDistVec=StationaryDistVec(SortedValues_index);
+            SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedStationaryDistVec)>0.5));
+            % Standard Deviation
+            SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
         else
-            SSvalueParamsVec=CreateVectorFromParams(Parameters,SSvalueParamNames);
+            SSvalueParamsVec=CreateVectorFromParams(Parameters,SSvalueParamNames(i).Names);
+            Values=zeros(N_a,N_z);
             if l_d==0
-                Values=zeros(N_a,N_z);
                 for j1=1:N_a
-                    a_ind=ind2sub_homemade([n_a],j1);
+                    a_ind=ind2sub_homemade_gpu([n_a],j1);
                     for jj1=1:l_a
                         if jj1==1
                             a_val(jj1)=a_grid(a_ind(jj1));
@@ -169,16 +146,16 @@ else
                             a_val(jj1)=a_grid(a_ind(jj1)+sum(n_a(1:jj1-1)));
                         end
                     end
-                    for j2=1:N_z
-                        s_ind=ind2sub_homemade([n_z],j2);
+                    for j2=1:N_s
+                        z_ind=ind2sub_homemade_gpu([n_z],j2);
                         for jj2=1:l_z
                             if jj2==1
-                                s_val(jj2)=z_grid(s_ind(jj2));
+                                z_val(jj2)=z_grid(z_ind(jj2));
                             else
-                                s_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                                z_val(jj2)=z_grid(z_ind(jj2)+sum(n_z(1:jj2-1)));
                             end
                         end
-                        aprime_ind=PolicyIndexesKron(l_d+1:l_d+l_a,j1,j2);
+                        [aprime_ind]=PolicyIndexes(:,j1,j2);
                         for kk2=1:l_a
                             if kk2==1
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2));
@@ -186,27 +163,12 @@ else
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2)+sum(n_a(1:kk2-1)));
                             end
                         end
-                        Values(j1,j2)=SSvaluesFn{i}(aprime_val,a_val,s_val,SSvalueParamsVec);
+                        Values(j1,j2)=SSvaluesFn{i}(aprime_val,a_val,z_val,SSvalueParamsVec);
                     end
                 end
-                
-                Values=reshape(Values,[N_a*N_z,1]);
-                
-                %Mean
-                SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
-                
-                %Median
-                [SortedValues,SortedValues_index] = sort(Values);
-                SortedSteadyStateDistVec=StationaryDistVec(SortedValues_index);
-                
-                SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedSteadyStateDistVec)>0.5));
-                
-                %Standard Deviation
-                SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
             else
-                Values=zeros(N_a,N_z);
                 for j1=1:N_a
-                    a_ind=ind2sub_homemade([n_a],j1);
+                    a_ind=ind2sub_homemade_gpu([n_a],j1);
                     for jj1=1:l_a
                         if jj1==1
                             a_val(jj1)=a_grid(a_ind(jj1));
@@ -215,23 +177,23 @@ else
                         end
                     end
                     for j2=1:N_z
-                        s_ind=ind2sub_homemade([n_z],j2);
+                        z_ind=ind2sub_homemade_gpu([n_z],j2);
                         for jj2=1:l_z
                             if jj2==1
-                                s_val(jj2)=z_grid(s_ind(jj2));
+                                z_val(jj2)=z_grid(z_ind(jj2));
                             else
-                                s_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                                z_val(jj2)=z_grid(z_ind(jj2)+sum(n_z(1:jj2-1)));
                             end
                         end
-                        d_ind=PolicyIndexesKron(1:l_d,j1,j2);
-                        for kk=1:l_d
-                            if kk==1
-                                d_val(kk)=d_grid(d_ind(kk));
+                        d_ind=PolicyIndexes(1:l_d,j1,j2);
+                        aprime_ind=PolicyIndexes(l_d+1:l_d+l_a,j1,j2);
+                        for kk1=1:l_d
+                            if kk1==1
+                                d_val(kk1)=d_grid(d_ind(kk1));
                             else
-                                d_val(kk)=d_grid(d_ind(kk)+sum(n_d(1:kk-1)));
+                                d_val(kk1)=d_grid(d_ind(kk1)+sum(n_d(1:kk1-1)));
                             end
                         end
-                        aprime_ind=PolicyIndexesKron(l_d+1:l_d+l_a,j1,j2);
                         for kk2=1:l_a
                             if kk2==1
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2));
@@ -239,26 +201,24 @@ else
                                 aprime_val(kk2)=a_grid(aprime_ind(kk2)+sum(n_a(1:kk2-1)));
                             end
                         end
-                        Values(j1,j2)=SSvaluesFn{i}(d_val,aprime_val,a_val,s_val,SSvalueParamsVec);
+                        Values(j1,j2)=SSvaluesFn{i}(d_val,aprime_val,a_val,z_val,SSvalueParamsVec(:));
                     end
                 end
-                
-                Values=reshape(Values,[N_a*N_z,1]);
-                
-                %Mean
-                SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
-                
-                %Median
-                [SortedValues,SortedValues_index] = sort(Values);
-                SortedSteadyStateDistVec=StationaryDistVec(SortedValues_index);
-                
-                SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedSteadyStateDistVec)>0.5));
-                
-                %Standard Deviation
-                SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
             end
+            Values=reshape(Values,[N_a*N_z,1]);
+            
+            % Mean
+            SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
+            % Median
+            [SortedValues,SortedValues_index] = sort(Values);
+            SortedStationaryDistVec=StationaryDistVec(SortedValues_index);
+            SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedStationaryDistVec)>0.5));
+            % Standard Deviation
+            SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
         end
+        
     end
     
 end
+
 
--- SteadyStateStatistics/SSvalues_MeanMedianStdDev_Case2.m
+++ SteadyStateStatistics/SSvalues_MeanMedianStdDev_Case2.m
@@ -1,77 +1,135 @@
-function SSvalues_MeanMedianStdDev=SSvalues_MeanMedianStdDev_Case2(SteadyStateDist, PolicyIndexes, SSvaluesFn, n_d, n_a, n_s, d_grid, a_grid, s_grid, pi_s,p_val,Parallel)
-
-if nargin<12 %Default is to assume SteadyStateDist exists on the gpu
-    Parallel=2;
-end
+function SSvalues_MeanMedianStdDev=SSvalues_MeanMedianStdDev_Case2(StationaryDist, PolicyIndexes, SSvaluesFn, Parameters,SSvalueParamNames, n_d, n_a, n_z, d_grid, a_grid, z_grid,Parallel)
+% Evaluates the mean value (weighted sum/integral), median value, and standard deviation for each element of SSvaluesFn
 
 l_d=length(n_d);
 l_a=length(n_a);
-l_s=length(n_s);
+l_z=length(n_z);
+
 N_a=prod(n_a);
-N_s=prod(n_s);
+N_z=prod(n_z);
 
 if Parallel==2
-    PolicyIndexesKron=gather(reshape(PolicyIndexes,[l_d,N_a,N_s]));
-    SteadyStateDistVec=gather(reshape(SteadyStateDist,[N_a*N_s,1]));
-    d_grid=gather(d_grid);
-    a_grid=gather(a_grid);
-    s_grid=gather(s_grid);
-    pi_s=gather(pi_s);
-else 
-    PolicyIndexesKron=reshape(PolicyIndexes,[l_d,N_a,N_s]);
-    SteadyStateDistVec=reshape(SteadyStateDist,[N_a*N_s,1]);
-end
+    SSvalues_MeanMedianStdDev=zeros(length(SSvaluesFn),3,'gpuArray'); % 3 columns: Mean, Median, and Standard Deviation
 
-SSvalues_MeanMedianStdDev=zeros(length(SSvaluesFn),3); % 3 columns: Mean, Median, and Standard Deviation
-d_val=zeros(l_d,1);
-a_val=zeros(l_a,1);
-s_val=zeros(l_s,1);
-for i=1:length(SSvaluesFn)
-    Values=zeros(N_a,N_s);
-    for j1=1:N_a
-        a_ind=ind2sub_homemade([n_a],j1);
-        for jj1=1:l_a
-            if jj1==1
-                a_val(jj1)=a_grid(a_ind(jj1));
-            else
-                a_val(jj1)=a_grid(a_ind(jj1)+sum(n_a(1:jj1-1)));
-            end
+    StationaryDistVec=reshape(StationaryDist,[N_a*N_z,1]);
+    
+    PolicyValues=PolicyInd2Val_Case2(PolicyIndexes,n_d,n_a,n_z,d_grid,Parallel);
+    permuteindexes=[1+(1:1:(l_a+l_z)),1];
+    PolicyValuesPermute=permute(PolicyValues,permuteindexes); %[n_a,n_s,l_d+l_a]
+    
+    for i=1:length(SSvaluesFn)
+        % Includes check for cases in which no parameters are actually required
+        if isempty(SSvalueParamNames)% || strcmp(SSvalueParamNames(1),'')) % check for 'SSvalueParamNames={}'
+            SSvalueParamsVec=[];
+        else
+            SSvalueParamsVec=CreateVectorFromParams(Parameters,SSvalueParamNames(i).Names);
         end
-        for j2=1:N_s
-            s_ind=ind2sub_homemade([n_s],j2);
-            for jj2=1:l_s
-                if jj2==1
-                    s_val(jj2)=s_grid(s_ind(jj2));
-                else
-                    s_val(jj2)=s_grid(s_ind(jj2)+sum(n_s(1:jj2-1)));
-                end
-            end
-            d_ind=PolicyIndexesKron(:,j1,j2);
-            for kk=1:l_d
-                if kk==1
-                    d_val(kk)=d_grid(d_ind(kk));
-                else
-                    d_val(kk)=d_grid(d_ind(kk)+sum(n_d(1:kk-1)));
-                end
-            end
-            Values(j1,j2)=SSvaluesFn{i}(d_val,a_val,s_val,pi_s,p_val);
-        end
+        Values=ValuesOnSSGrid_Case2(SSvaluesFn{i}, SSvalueParamsVec,PolicyValuesPermute,n_d,n_a,n_z,a_grid,z_grid,Parallel);
+        Values=reshape(Values,[N_a*N_z,1]);
+        % Mean
+        SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
+        % Median
+        [SortedValues,SortedValues_index] = sort(Values);
+        SortedStationaryDistVec=StationaryDistVec(SortedValues_index);
+        SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedStationaryDistVec)>0.5));
+        % Standard Deviation
+        SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
     end
+else
+    SSvalues_MeanMedianStdDev=zeros(length(SSvaluesFn),3); % 3 columns: Mean, Median, and Standard Deviation
+    d_val=zeros(l_d,1);
+    a_val=zeros(l_a,1);
+    z_val=zeros(l_z,1);
+    StationaryDistVec=reshape(StationaryDist,[N_a*N_z,1]);
     
-    Values=reshape(Values,[N_a*N_s,1]);
-    
-    %Mean
-    SSvalues_MeanMedianStdDev(i,1)=sum(Values.*SteadyStateDistVec);
-    
-    %Median
-    [SortedValues,SortedValues_index] = sort(Values);
-    SortedSteadyStateDistVec=SteadyStateDistVec(SortedValues_index);
-    
-    SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedSteadyStateDistVec)>0.5));
-    
-    %Standard Deviation
-    SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(SteadyStateDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_s,1)).^2)));
-    
+    for i=1:length(SSvaluesFn)
+        % Includes check for cases in which no parameters are actually required
+        if isempty(SSvalueParamNames) % check for 'SSvalueParamNames={}'
+            Values=zeros(N_a,N_z);
+            for j1=1:N_a
+                a_ind=ind2sub_homemade_gpu([n_a],j1);
+                for jj1=1:l_a
+                    if jj1==1
+                        a_val(jj1)=a_grid(a_ind(jj1));
+                    else
+                        a_val(jj1)=a_grid(a_ind(jj1)+sum(n_a(1:jj1-1)));
+                    end
+                end
+                for j2=1:N_z
+                    s_ind=ind2sub_homemade_gpu([n_z],j2);
+                    for jj2=1:l_z
+                        if jj2==1
+                            z_val(jj2)=z_grid(s_ind(jj2));
+                        else
+                            z_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                        end
+                    end
+                    d_ind=PolicyIndexes(1:l_d,j1,j2);
+                    for kk1=1:l_d
+                        if kk1==1
+                            d_val(kk1)=d_grid(d_ind(kk1));
+                        else
+                            d_val(kk1)=d_grid(d_ind(kk1)+sum(n_d(1:kk1-1)));
+                        end
+                    end
+                    Values(j1,j2)=SSvaluesFn{i}(d_val,a_val,z_val);
+                end
+            end
+            Values=reshape(Values,[N_a*N_z,1]);
+            
+            % Mean
+            SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
+            % Median
+            [SortedValues,SortedValues_index] = sort(Values);
+            SortedStationaryDistVec=StationaryDistVec(SortedValues_index);
+            SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedStationaryDistVec)>0.5));
+            % Standard Deviation
+            SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
+        else
+            SSvalueParamsVec=CreateVectorFromParams(Parameters,SSvalueParamNames(i).Names);
+            Values=zeros(N_a,N_z);
+            for j1=1:N_a
+                a_ind=ind2sub_homemade_gpu([n_a],j1);
+                for jj1=1:l_a
+                    if jj1==1
+                        a_val(jj1)=a_grid(a_ind(jj1));
+                    else
+                        a_val(jj1)=a_grid(a_ind(jj1)+sum(n_a(1:jj1-1)));
+                    end
+                end
+                for j2=1:N_z
+                    s_ind=ind2sub_homemade_gpu([n_z],j2);
+                    for jj2=1:l_z
+                        if jj2==1
+                            z_val(jj2)=z_grid(s_ind(jj2));
+                        else
+                            z_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                        end
+                    end
+                    d_ind=PolicyIndexes(1:l_d,j1,j2);
+                    for kk1=1:l_d
+                        if kk1==1
+                            d_val(kk1)=d_grid(d_ind(kk1));
+                        else
+                            d_val(kk1)=d_grid(d_ind(kk1)+sum(n_d(1:kk1-1)));
+                        end
+                    end
+                    Values(j1,j2)=SSvaluesFn{i}(d_val,a_val,z_val,SSvalueParamsVec);
+                end
+            end
+            Values=reshape(Values,[N_a*N_z,1]);
+            
+            % Mean
+            SSvalues_MeanMedianStdDev(i,1)=sum(Values.*StationaryDistVec);
+            % Median
+            [SortedValues,SortedValues_index] = sort(Values);
+            SortedStationaryDistVec=StationaryDistVec(SortedValues_index);
+            SSvalues_MeanMedianStdDev(i,2)=min(SortedValues(cumsum(SortedStationaryDistVec)>0.5));
+            % Standard Deviation
+            SSvalues_MeanMedianStdDev(i,3)=sqrt(sum(StationaryDistVec.*((Values-SSvalues_MeanMedianStdDev(i,1).*ones(N_a*N_z,1)).^2)));
+        end
+        
+    end
 end
 
 end
