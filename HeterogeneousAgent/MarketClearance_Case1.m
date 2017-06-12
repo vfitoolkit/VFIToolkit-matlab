c54c3368f6a455a28ea5b4734cd1775db2ad7de0
robertdkirkby <robertdkirkby@gmail.com>
2017-06-12 19:29:11 +1200

    MarketClearance corrected to work with new parameter setup in market clearance conditions

--- HeterogeneousAgent/MarketClearance_Case1.m
+++ HeterogeneousAgent/MarketClearance_Case1.m
@@ -5,12 +5,16 @@ MarketClearanceVec=ones(1,length(MarketClearanceEqns),'gpuArray')*Inf;
 for i=1:length(MarketClearanceEqns)
     if isempty(MarketClearanceParamNames(i).Names)  % check for 'MarketClearanceParamNames(i).Names={}'
         MarketClearanceParamsVec=gpuArray([]);
+        MarketClearanceVec(i)=MarketClearanceEqns{i}(SSvalues_AggVars, p, MarketClearanceParamsVec);
     else
         MarketClearanceParamsVec=gpuArray(CreateVectorFromParams(Parameters,MarketClearanceParamNames(i).Names));
-    end
-    
-    MarketClearanceVec(i)=MarketClearanceEqns{i}(SSvalues_AggVars, p, MarketClearanceParamsVec);
-end
+        MarketClearanceParamsCell=cell(length(MarketClearanceParamsVec),1);
+        for jj=1:length(MarketClearanceParamsVec)
+            MarketClearanceParamsCell(jj,1)={MarketClearanceParamsVec(jj)};
+        end
 
+        MarketClearanceVec(i)=MarketClearanceEqns{i}(SSvalues_AggVars, p, MarketClearanceParamsCell{:});
+    end
+end
 
 end
--- HeterogeneousAgent/MarketClearance_Case2.m
+++ HeterogeneousAgent/MarketClearance_Case2.m
@@ -5,11 +5,17 @@ MarketClearanceVec=ones(1,length(MarketClearanceEqns),'gpuArray')*Inf;
 for i=1:length(MarketClearanceEqns)
     if isempty(MarketClearanceParamNames(i).Names)  % check for 'MarketClearanceParamNames(i).Names={}'
         MarketClearanceParamsVec=gpuArray([]);
+        MarketClearanceVec(i)=MarketClearanceEqns{i}(SSvalues_AggVars, p, MarketClearanceParamsVec);
     else
         MarketClearanceParamsVec=gpuArray(CreateVectorFromParams(Parameters,MarketClearanceParamNames(i).Names));
+        MarketClearanceParamsCell=cell(length(MarketClearanceParamsVec),1);
+        for jj=1:length(MarketClearanceParamsVec)
+            MarketClearanceParamsCell(jj,1)={MarketClearanceParamsVec(jj)};
+        end
+
+        MarketClearanceVec(i)=MarketClearanceEqns{i}(SSvalues_AggVars, p, MarketClearanceParamsCell{:});
     end
     
-    MarketClearanceVec(i)=MarketClearanceEqns{i}(SSvalues_AggVars, p, MarketClearanceParamsVec);
 end
 
 end
