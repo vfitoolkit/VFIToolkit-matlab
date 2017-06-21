d829a0f5e80968f2dbf6fa7502c9a93ca19f48cf
robertdkirkby <robertdkirkby@gmail.com>
2017-06-22 09:26:03 +1200

    minor improvement

--- SubCodes/KronPolicyIndexes/KronPolicyIndexes_FHorz_Case1.m
+++ SubCodes/KronPolicyIndexes/KronPolicyIndexes_FHorz_Case1.m
@@ -5,8 +5,14 @@ function PolicyKron=KronPolicyIndexes_FHorz_Case1(Policy, n_d, n_a, n_z,N_j,opti
 %Output: Policy=zeros(2,N_a,N_z,N_j); %first dim indexes the optimal choice for d and aprime rest of dimensions a,z 
 %                       (N_a,N_z,N_j) if there is no d
 
+
 N_a=prod(n_a);
 N_z=prod(n_z);
+% 
+% size(Policy)
+% whos Policy n_d n_a n_z N_j
+% size(Policy,1)
+% [N_a,N_z,N_j]
 
 Policy=reshape(Policy,[size(Policy,1),N_a,N_z,N_j]);
 
