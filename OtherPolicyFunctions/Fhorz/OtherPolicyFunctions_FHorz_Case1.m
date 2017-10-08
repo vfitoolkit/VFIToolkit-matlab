5f29f06aeef46d5952af86a1a1ae743e0b1268be
robertdkirkby <robertdkirkby@gmail.com>
2017-10-09 12:58:16 +1300

    OtherPolicyFunctions_Fhorz_Case1 allows you to calculate other policy functions. Is essentially just a wrapper for calling ValuesOnSSGrid_Case1.

---
+++ OtherPolicyFunctions/Fhorz/OtherPolicyFunctions_FHorz_Case1.m
@@ -0,0 +1,146 @@
+function OtherPolicies=OtherPolicyFunctions_FHorz_Case1(PolicyIndexes, ValuesFn, ValuesFnsParamNames,Parameters,n_d,n_a,n_z,N_j,d_grid,a_grid,z_grid,Parallel)
+% Creates OtherPolicies, which is the same thing as Policy but rather than being for the underlying policies it evaluates those in ValuesFn.
+
+% Is doing essentially the same thing as SSaggVars but now it just returns
+% the whole grid rather than taking weighted sum.
+
+if n_d(1)==0
+    l_d=0;
+else
+    l_d=length(n_d);
+end
+l_a=length(n_a);
+l_z=length(n_z);
+N_a=prod(n_a);
+N_z=prod(n_z);
+sizePolicyIndexes=size(PolicyIndexes);
+
+if Parallel==2
+    OtherPolicies=zeros([length(ValuesFn),sizePolicyIndexes(2:end)],'gpuArray');
+        
+    PolicyValues=PolicyInd2Val_FHorz_Case1(PolicyIndexes,n_d,n_a,n_z,N_j,d_grid,a_grid, Parallel);
+    permuteindexes=[1+(1:1:(l_a+l_z)),1,1+l_a+l_z+1];
+    PolicyValuesPermute=permute(PolicyValues,permuteindexes); %[n_a,n_z,l_d+l_a,N_j]
+    
+    PolicyValuesPermuteVec=reshape(PolicyValuesPermute,[N_a*N_z*(l_d+l_a),N_j]);
+    for i=1:length(ValuesFn)
+        Values=nan(N_a*N_z,N_j,'gpuArray');
+        for jj=1:N_j
+            % Includes check for cases in which no parameters are actually required
+            if isempty(ValuesFnsParamNames)% || strcmp(SSvalueParamNames(1),'')) % check for 'SSvalueParamNames={}'
+                SSvalueParamsVec=[];
+            else
+                SSvalueParamsVec=CreateVectorFromParams(Parameters,ValuesFnsParamNames(i).Names,jj);
+            end
+            Values(:,jj)=ValuesOnSSGrid_Case1(ValuesFn{i}, SSvalueParamsVec,reshape(PolicyValuesPermuteVec(:,jj),[n_a,n_z,l_d+l_a]),n_d,n_a,n_z,a_grid,z_grid,Parallel);
+        end
+        %         Values=reshape(Values,[N_a*N_z,N_j]);
+        OtherPolicies(i,:,:)=shiftdim(Values,-1);
+    end
+    
+else
+    OtherPolicies=zeros(length(ValuesFn),1);
+    d_val=zeros(l_d,1);
+    aprime_val=zeros(l_a,1);
+    a_val=zeros(l_a,1);
+    z_val=zeros(l_z,1);
+    
+    d_grid=gather(d_grid);
+    a_grid=gather(a_grid);
+    z_grid=gather(z_grid);
+    
+    for i=1:length(ValuesFn)
+        Values=zeros(N_a,N_z,N_j);
+        if l_d==0
+            for j1=1:N_a
+                a_ind=ind2sub_homemade_gpu([n_a],j1);
+                for jj1=1:l_a
+                    if jj1==1
+                        a_val(jj1)=a_grid(a_ind(jj1));
+                    else
+                        a_val(jj1)=a_grid(a_ind(jj1)+sum(n_a(1:jj1-1)));
+                    end
+                end
+                for j2=1:N_z
+                    s_ind=ind2sub_homemade_gpu([n_z],j2);
+                    for jj2=1:l_z
+                        if jj2==1
+                            z_val(jj2)=z_grid(s_ind(jj2));
+                        else
+                            z_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                        end
+                    end
+                    for jj=1:N_j
+                        [aprime_ind]=PolicyIndexes(:,j1,j2,jj);
+                        for kk2=1:l_a
+                            if kk2==1
+                                aprime_val(kk2)=a_grid(aprime_ind(kk2));
+                            else
+                                aprime_val(kk2)=a_grid(aprime_ind(kk2)+sum(n_a(1:kk2-1)));
+                            end
+                        end
+                        % Includes check for cases in which no parameters are actually required
+                        if isempty(ValuesFnsParamNames) %|| strcmp(SSvalueParamNames(1),'')) % check for 'SSvalueParamNames={}'
+                            Values(j1,j2,jj)=ValuesFn{i}(d_val,aprime_val,a_val,z_val);
+                        else
+                            SSvalueParamsVec=CreateVectorFromParams(Parameters,ValuesFnsParamNames(i).Names,jj);
+                            Values(j1,j2,jj)=ValuesFn{i}(d_val,aprime_val,a_val,z_val,SSvalueParamsVec);
+                        end
+                    end
+                end
+            end
+        else
+            for j1=1:N_a
+                a_ind=ind2sub_homemade_gpu([n_a],j1);
+                for jj1=1:l_a
+                    if jj1==1
+                        a_val(jj1)=a_grid(a_ind(jj1));
+                    else
+                        a_val(jj1)=a_grid(a_ind(jj1)+sum(n_a(1:jj1-1)));
+                    end
+                end
+                for j2=1:N_z
+                    s_ind=ind2sub_homemade_gpu([n_z],j2);
+                    for jj2=1:l_z
+                        if jj2==1
+                            z_val(jj2)=z_grid(s_ind(jj2));
+                        else
+                            z_val(jj2)=z_grid(s_ind(jj2)+sum(n_z(1:jj2-1)));
+                        end
+                    end
+                    for jj=1:N_j
+                        d_ind=PolicyIndexes(1:l_d,j1,j2,jj);
+                        aprime_ind=PolicyIndexes(l_d+1:l_d+l_a,j1,j2,jj);
+                        for kk1=1:l_d
+                            if kk1==1
+                                d_val(kk1)=d_grid(d_ind(kk1));
+                            else
+                                d_val(kk1)=d_grid(d_ind(kk1)+sum(n_d(1:kk1-1)));
+                            end
+                        end
+                        for kk2=1:l_a
+                            if kk2==1
+                                aprime_val(kk2)=a_grid(aprime_ind(kk2));
+                            else
+                                aprime_val(kk2)=a_grid(aprime_ind(kk2)+sum(n_a(1:kk2-1)));
+                            end
+                        end
+                        % Includes check for cases in which no parameters are actually required
+                        if isempty(ValuesFnsParamNames) %|| strcmp(SSvalueParamNames(1),'')) % check for 'SSvalueParamNames={}'
+                            Values(j1,j2,jj)=ValuesFn{i}(d_val,aprime_val,a_val,z_val);
+                        else
+                            SSvalueParamsVec=CreateVectorFromParams(Parameters,ValuesFnsParamNames(i).Names,jj);
+                            Values(j1,j2,jj)=ValuesFn{i}(d_val,aprime_val,a_val,z_val,SSvalueParamsVec);
+                        end
+                    end
+                end
+            end
+        end
+        Values=reshape(Values,[N_a*N_z*N_j,1]);
+        OtherPolicies(i,:,:)=shiftdim(Values,-1);
+    end
+    
+end
+
+
+end
