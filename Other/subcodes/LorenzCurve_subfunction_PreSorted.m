8327d5fdd050fb11ee8e2dbbb3a58366b4917cd5
robertdkirkby <robertdkirkby@gmail.com>
2018-08-08 06:36:18 +1200

    minor tweak

--- Other/subcodes/LorenzCurve_subfunction_PreSorted.m
+++ Other/subcodes/LorenzCurve_subfunction_PreSorted.m
@@ -1,15 +1,20 @@
 function LorenzCurve=LorenzCurve_subfunction_PreSorted(SortedWeightedValues,CumSumSortedWeights,npoints)
 
+% I have considered making this an option to be set but for now is just hardcoded.
+% Order to round to avoid numerical error
+options.ordertoroundtoavoidnumericalerror=10;
+
 % Calculate the 'age conditional' lorenz curve
 %We now want to use interpolation, but this won't work unless all values in CumSumSortedWeights are distinct. So we now remove
 %any duplicates (ie. points of zero probability mass/density). We then have to remove the corresponding points of SortedValues. Since we
 %are just looking for 100 points to make up our cdf I round all variables to 10 decimal points before checking for uniqueness (Do
 %this because otherwise rounding in the ~12th decimal place was causing problems with vector not being sorted as strictly increasing.
-[~,UniqueIndex] = unique(floor(CumSumSortedWeights*10^10),'first');
+[~,UniqueIndex] = unique(round(CumSumSortedWeights*10^options.ordertoroundtoavoidnumericalerror),'first');
 CumSumSortedStationaryDistVec_NoDuplicates=CumSumSortedWeights(sort(UniqueIndex));
 SortedWeightedValues_NoDuplicates=SortedWeightedValues(sort(UniqueIndex));
 CumSumSortedWeightedValues_NoDuplicates=cumsum(SortedWeightedValues_NoDuplicates);
 InverseCDF_xgrid=gpuArray(1/npoints:1/npoints:1);
+
 InverseCDF_SSvalues=interp1(CumSumSortedStationaryDistVec_NoDuplicates,CumSumSortedWeightedValues_NoDuplicates, InverseCDF_xgrid);
 % interp1 cannot work for the point of InverseCDF_xgrid=1 (gives NaN). Since we have already sorted and removed duplicates this will just be the last
 % point so we can just grab it directly.
