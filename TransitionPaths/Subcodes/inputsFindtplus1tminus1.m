bee893f3e7fabdb9effd679271d950bf39c97258
robertdkirkby <robertdkirkby@gmail.com>
2021-12-21 17:15:13 +1300

    Improved transition path features

---
+++ TransitionPaths/Subcodes/inputsFindtplus1tminus1.m
@@ -0,0 +1,96 @@
+function [tplus1priceNames,tminus1priceNames,tminus1AggVarsNames,tplus1pricePathkk]=inputsFindtplus1tminus1(FnsToEvaluate,GeneralEqmEqns,PricePathNames)
+% Subscript that is used to determine if there are any '_tminus1' or
+% '_tplus1' variables used as inputs to FnsToEvaluate or GeneralEqmEqns
+% (Used as part of transition path codes)
+
+% Check for using _tminus1 (for price or AggVars) or _tplus1
+FnInputNames={};
+ninputs=0;
+% Get all the input names for FnsToEvaluate
+AggVarNames=fieldnames(FnsToEvaluate);
+for ff=1:length(AggVarNames)
+    temp=getAnonymousFnInputNames(FnsToEvaluate.(AggVarNames{ff}));
+    tempninputs=length(temp);
+%     FnInputNames{ninputs+1:ninputs+tempninputs}=temp; % Note, this will include the (d,aprime,a,z), but that is irrelevant to our current purposes
+    FnInputNames={FnInputNames{:},temp{:}}; % Note, this will include the (d,aprime,a,z), but that is irrelevant to our current purposes
+    ninputs=ninputs+tempninputs;
+end
+% Get all the input names for GeneralEqmEqns
+GEeqnNames=fieldnames(GeneralEqmEqns);
+for ff=1:length(GEeqnNames)
+    temp=getAnonymousFnInputNames(GeneralEqmEqns.(GEeqnNames{ff}));
+    tempninputs=length(temp);
+%     FnInputNames{ninputs+1:ninputs+tempninputs}=temp;  % Note, this will include the (d,aprime,a,z), but that is irrelevant to our current purposes
+    FnInputNames={FnInputNames{:},temp{:}}; % Note, this will include the (d,aprime,a,z), but that is irrelevant to our current purposes
+    ninputs=ninputs+tempninputs;
+end
+% Now, get rid of all the duplicates
+FnInputNames=unique(FnInputNames);
+% Find any which are _tplus1 or _tminus1
+tplus1Names={};
+ntplus1=0;
+tminus1Names={};
+ntminus1=0;
+for ii=1:length(FnInputNames)
+    if length(FnInputNames{ii})>7
+        temp=FnInputNames{ii};
+        if strcmp(temp(end-6:end),'_tplus1')
+            ntplus1=ntplus1+1;
+            tplus1Names{ntplus1}=temp(1:end-7); % Drop the _tminus1 from name
+        end
+        if length(FnInputNames{ii})>8
+            if strcmp(temp(end-7:end),'_tminus1')
+                ntminus1=ntminus1+1;
+                tminus1Names{ntminus1}=temp(1:end-8); % Drop the _tminus1 from name
+            end
+        end
+    end
+end
+tplus1UsedAsPriceOrAggVar=zeros(ntplus1,1); % I replace the zeros with ones as I find them
+tminus1UsedAsPriceOrAggVar=zeros(ntminus1,1); % I replace the zeros with ones as I find them
+tplus1priceNames={}; ntplus1prices=0;
+tminus1priceNames={}; ntminus1prices=0;
+tminus1AggVarsNames={}; ntminus1AggVars=0;
+tplus1pricePathkk=[];
+% Check that they are prices, otherwise error
+for kk=1:length(PricePathNames)
+    for ii=1:ntplus1
+        if strcmp(tplus1Names{ii},PricePathNames{kk})
+            ntplus1prices=ntplus1prices+1;
+            tplus1priceNames{ntplus1prices}=tplus1Names{ii};
+            tplus1UsedAsPriceOrAggVar(ii)=1;
+            tplus1pricePathkk(ii)=kk;
+        end
+    end
+    for ii=1:ntminus1
+        if strcmp(tminus1Names{ii},PricePathNames{kk})
+            ntminus1prices=ntminus1prices+1;
+            tminus1priceNames{ntminus1prices}=tminus1Names{ii};
+            tminus1UsedAsPriceOrAggVar(ii)=1;
+        end
+    end
+end
+for ff=1:length(AggVarNames)
+    for ii=1:ntminus1
+        if strcmp(tminus1Names{ii},AggVarNames{kk})
+            ntminus1AggVars=ntminus1AggVars+1;
+            tminus1AggVarsNames{ntminus1AggVars}=tminus1Names{ii};
+            tminus1UsedAsPriceOrAggVar(ii)=1;
+        end
+    end
+end
+% Check that they have all been use, otherwise error
+if prod(tplus1UsedAsPriceOrAggVar)==0
+    fprintf('ERROR: FnsToEvaluate or GeneralEqmEqns are trying to use a _tplus1 input that is NOT a prices \n')
+    dbstack
+    return
+elseif prod(tminus1UsedAsPriceOrAggVar)==0
+    fprintf('ERROR: FnsToEvaluate or GeneralEqmEqns are trying to use a _tminus1 input that NEITHER a price NOR an an aggregate (determined by FnsToEvaluate) \n')
+    dbstack
+    return
+end
+
+
+% tplus1pricePathkk
+
+end
