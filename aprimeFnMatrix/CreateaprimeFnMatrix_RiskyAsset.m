b093c3d7e0e0a11de3acab2d60bd6b7522eba44f
robertdkirkby <robertdkirkby@gmail.com>
2023-10-18 09:02:26 +1300

    minor

---
+++ aprimeFnMatrix/CreateaprimeFnMatrix_RiskyAsset.m
@@ -0,0 +1,175 @@
+function [aprimeIndexes,aprimeProbs]=CreateaprimeFnMatrix_RiskyAsset(aprimeFn, n_d, n_a, n_u, d_grid, a_grid, u_grid, aprimeFnParams, aprimeIndexAsColumn)
+% Note: aprimeIndex is [N_a*N_u,1], whereas aprimeProbs is [N_a,N_u]
+%
+% Creates the grid points and their 'interpolation' probabilities
+% Note: aprimeIndexes is always the 'lower' point (the upper points are
+% just aprimeIndexes+1, so no need to waste memory storing them), and the
+% aprimeProbs are the probability of this lower point (prob of upper point
+% is just 1 minus this).
+
+ParamCell=cell(length(aprimeFnParams),1);
+for ii=1:length(aprimeFnParams)
+    if size(aprimeFnParams(ii))~=[1,1]
+        error('Using GPU for the return fn does not allow for any of aprimeFn parameters to be anything but a scalar')
+    end
+    ParamCell(ii,1)={aprimeFnParams(ii)};
+end
+
+N_d=prod(n_d);
+N_u=prod(n_u);
+
+l_d=length(n_d);
+if N_d==0
+    l_d=0;
+end
+l_a=length(n_a);
+l_u=length(n_u);
+if l_d>4
+    error('ERROR: Using GPU for the return fn does not allow for more than four of d variable (you have length(n_d)>4)')
+end
+if l_u>5
+    error('ERROR: Using GPU for the return fn does not allow for more than five of u variable (you have length(n_u)>5)')
+end
+
+if nargin(aprimeFn)~=l_d+l_u+length(aprimeFnParams)
+    error('ERROR: Number of inputs to aprimeFn does not fit with size of aprimeFnParams')
+end
+
+if l_d>=1
+    d1vals=d_grid(1:n_d(1));
+    if l_d>=2
+        d2vals=shiftdim(d_grid(n_d(1)+1:sum(n_d(1:2))),-1);
+        if l_d>=3
+            d3vals=shiftdim(d_grid(sum(n_d(1:2))+1:sum(n_d(1:3))),-2);
+            if l_d>=4
+                d4vals=shiftdim(d_grid(sum(n_d(1:3))+1:sum(n_d(1:4))),-3);
+            end
+        end
+    end
+end
+if all(size(u_grid)==[sum(n_u),1]) % kroneker product u_grid
+    if l_u>=1
+        u1vals=shiftdim(u_grid(1:n_u(1)),-l_d);
+        if l_u>=2
+            u2vals=shiftdim(u_grid(n_u(1)+1:n_u(1)+n_u(2)),-l_d-1);
+            if l_u>=3
+                u3vals=shiftdim(u_grid(sum(n_u(1:2))+1:sum(n_u(1:3))),-l_d-2);
+                if l_u>=4
+                    u4vals=shiftdim(u_grid(sum(n_u(1:3))+1:sum(n_u(1:4))),-l_d-3);
+                    if l_u>=5
+                        u5vals=shiftdim(u_grid(sum(n_u(1:4))+1:sum(n_u(1:5))),-l_d-4);
+                    end
+                end
+            end
+        end
+    end
+elseif all(size(u_grid)==[prod(n_u),l_u]) % joint u_grid
+    if l_u>=1
+        u1vals=shiftdim(u_grid(:,1),-l_d);
+        if l_u>=2
+            u2vals=shiftdim(u_grid(:,2),-l_d);
+            if l_u>=3
+                u3vals=shiftdim(u_grid(:,3),-l_d);
+                if l_u>=4
+                    u4vals=shiftdim(u_grid(:,4),-l_d);
+                    if l_u>=5
+                        u5vals=shiftdim(u_grid(:,5),-l_d);
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+if l_d==1
+    if l_u==1
+        d1vals(1,1,1,1)=d_grid(1); % Requires special treatment
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals, ParamCell{:});
+    elseif l_u==2
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals,u2vals, ParamCell{:});
+    elseif l_u==3
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals,u2vals,u3vals, ParamCell{:});
+    elseif l_u==4
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals,u2vals,u3vals,u4vals, ParamCell{:});
+    elseif l_u==5
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals,u2vals,u3vals,u4vals,u5vals, ParamCell{:});
+    end
+elseif l_d==2
+    if l_u==1
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals, u1vals, ParamCell{:});
+    elseif l_u==2
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals, u1vals,u2vals, ParamCell{:});
+    elseif l_u==3
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals, u1vals,u2vals,u3vals, ParamCell{:});
+    elseif l_u==4
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals, u1vals,u2vals,u3vals,u4vals, ParamCell{:});
+    elseif l_u==5
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals, u1vals,u2vals,u3vals,u4vals,u5vals, ParamCell{:});
+    end
+elseif l_d==3
+    if l_u==1
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals, u1vals, ParamCell{:});
+    elseif  l_u==2
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals, u1vals,u2vals, ParamCell{:});
+    elseif  l_u==3
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals, u1vals,u2vals,u3vals, ParamCell{:});
+    elseif  l_u==4
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals, u1vals,u2vals,u3vals,u4vals, ParamCell{:});
+    elseif  l_u==5
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals, u1vals,u2vals,u3vals,u4vals,u5vals, ParamCell{:});
+    end
+elseif l_d==4
+    if l_u==1
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals,d4vals, u1vals, ParamCell{:});
+    elseif l_u==2
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals,d4vals, u1vals,u2vals, ParamCell{:});
+    elseif l_u==3
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals,d4vals, u1vals,u2vals,u3vals, ParamCell{:});
+    elseif l_u==4
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals,d4vals, u1vals,u2vals,u3vals,u4vals, ParamCell{:});
+    elseif l_u==5
+        aprimeVals=arrayfun(aprimeFn, d1vals,d2vals,d3vals,d4vals, u1vals,u2vals,u3vals,u4vals,u5vals, ParamCell{:});
+    end
+end
+
+
+%% Calcuate grid indexes and probs from the values
+if l_a==1
+    aprimeVals=reshape(aprimeVals,[1,N_d*N_u]);
+
+    a_griddiff=a_grid(2:end)-a_grid(1:end-1); % Distance between point and the next point
+    
+    temp=a_grid-aprimeVals;
+    temp(temp>0)=1; % Equals 1 when a_grid is greater than aprimeVals
+    
+    [~,aprimeIndexes]=max(temp,[],1); % Keep the dimension corresponding to aprimeVals, minimize over the a_grid dimension
+    % Note, this is going to find the 'first' grid point such that aprimeVals is smaller than or equal to that grid point
+    % This is the 'upper' grid point
+        
+    % Switch to lower grid point index
+    aprimeIndexes=aprimeIndexes-1;
+    aprimeIndexes(aprimeIndexes==0)=1;
+        
+    % Now, find the probabilities
+    aprime_residual=aprimeVals'-a_grid(aprimeIndexes);
+    % Probability of the 'lower' points
+    aprimeProbs=1-aprime_residual./a_griddiff(aprimeIndexes);
+        
+    % Those points which tried to leave the top of the grid have probability 1 of the 'upper' point (0 of lower point)
+    offTopOfGrid=(aprimeVals>=a_grid(end));
+    aprimeProbs(offTopOfGrid)=0;
+    % Those points which tried to leave the bottom of the grid have probability 0 of the 'upper' point (1 of lower point)
+    offBottomOfGrid=(aprimeVals<=a_grid(1));
+    aprimeProbs(offBottomOfGrid)=1;
+    
+    if aprimeIndexAsColumn==1 % value fn codes want column, simulation codes want matrix
+%     aprimeIndexes=reshape(aprimeIndexes,[N_d*N_u,1]);
+        aprimeIndexes=aprimeIndexes'; % This is just doing the commented out reshape above
+    else
+        aprimeIndexes=reshape(aprimeIndexes,[N_d,N_u]);
+    end
+    aprimeProbs=reshape(aprimeProbs,[N_d,N_u]);
+end
+
+
---
+++ aprimeFnMatrix/CreateaprimePolicyRiskyAsset_Case1.m
@@ -0,0 +1,155 @@
+function [aprimeIndexes, aprimeProbs]=CreateaprimePolicyRiskyAsset_Case1(Policy,aprimeFn, whichisdforriskyasset, n_d, n_a, N_z, n_u, d_grid, a2_grid, u_grid, aprimeFnParams)
+% The input Policy will contain aprime (except for the risky asset)
+% and the decision variables (d2, and where applicable d1). The output is
+% just the Policy for aprime (for all endogenous states). As well as the
+% related probabilities.
+
+% Note: aprimeIndex is [N_a,N_z,N_u], so is aprimeProbs which only reports the probability for the lower grid point.
+%
+% Creates the grid points and their 'interpolation' probabilities.
+% Note: aprimeIndexes is always the 'lower' point (the upper points are
+% just aprimeIndexes+1, so no need to waste memory storing them), and the
+% aprimeProbs are the probability of this lower point (prob of upper point
+% is just 1 minus this).
+%
+%
+% Remark: This is like CreateRiskyAssetFnMatrix_Case1(), except
+% instead of looking at all possible d, we only care about those in Policy.
+% The Policy based ones are needed for simulation, while those for all
+% possible d were needed for value function (to find Policy).
+
+ParamCell=cell(length(aprimeFnParams),1);
+for ii=1:length(aprimeFnParams)
+    if size(aprimeFnParams(ii))~=[1,1]
+        error('Using GPU for the return fn does not allow for any of aprimeFn parameters to be anything but a scalar')
+    end
+    ParamCell(ii,1)={aprimeFnParams(ii)};
+end
+
+N_a=prod(n_a);
+N_u=prod(n_u);
+
+l_drisky=length(whichisdforriskyasset);
+l_u=length(n_u);
+
+if nargin(aprimeFn)~=l_drisky+l_u+length(aprimeFnParams)
+    error('Number of inputs to aprimeFn does not fit with size of aprimeFnParams')
+end
+
+% Note: hardcodes that there is just one u shock
+if l_u==1
+    u1vals=shiftdim(u_grid,-1);
+else
+    u_gridvals=CreateGridvals(n_u,u_grid,1);
+    u1vals=u_gridvals(:,1);
+    if l_u>=2
+        u2vals=u_gridvals(:,2);
+    else
+        error('Max of two u variables supported (contact if you need more)')
+    end
+end
+
+if N_z==0 % To save writing a seperate script for without z
+    if l_drisky>=1
+        if whichisdforriskyasset(1)==1
+            d1grid=d_grid(1:n_d(1));
+        else
+            d1grid=d_grid(sum(n_d(1:whichisdforriskyasset(1)-1))+1:sum(n_d(1:whichisdforriskyasset(1))));
+        end
+        d1vals=reshape(d1grid(Policy(whichisdforriskyasset(1),:)),[N_a,1]);
+        if l_drisky>=2
+            d2grid=d_grid(sum(n_d(1:whichisdforriskyasset(2)-1))+1:sum(n_d(1:whichisdforriskyasset(2))));
+            d2vals=reshape(d2grid(Policy(whichisdforriskyasset(2),:)),[N_a,1]);
+            if l_drisky>=3
+                d3grid=d_grid(sum(n_d(1:whichisdforriskyasset(3)-1))+1:sum(n_d(1:whichisdforriskyasset(3))));
+                d3vals=reshape(d3grid(Policy(whichisdforriskyasset(3),:)),[N_a,1]);
+                if l_drisky>=4
+                    d4grid=d_grid(sum(n_d(1:whichisdforriskyasset(4)-1))+1:sum(n_d(1:whichisdforriskyasset(4))));
+                    d4vals=reshape(d4grid(Policy(whichisdforriskyasset(4),:)),[N_a,1]);
+                end
+            end
+        end
+    end
+else
+    if l_drisky>=1
+        if whichisdforriskyasset(1)==1
+            d1grid=d_grid(1:n_d(1));
+        else
+            d1grid=d_grid(sum(n_d(1:whichisdforriskyasset(1)-1))+1:sum(n_d(1:whichisdforriskyasset(1))));
+        end
+        d1vals=reshape(d1grid(Policy(whichisdforriskyasset(1),:,:)),[N_a*N_z,1]);
+        if l_drisky>=2
+            d2grid=d_grid(sum(n_d(1:whichisdforriskyasset(2)-1))+1:sum(n_d(1:whichisdforriskyasset(2))));
+            d2vals=reshape(d2grid(Policy(whichisdforriskyasset(2),:,:)),[N_a*N_z,1]);
+            if l_drisky>=3
+                d3grid=d_grid(sum(n_d(1:whichisdforriskyasset(3)-1))+1:sum(n_d(1:whichisdforriskyasset(3))));
+                d3vals=reshape(d3grid(Policy(whichisdforriskyasset(3),:,:)),[N_a*N_z,1]);
+                if l_drisky>=4
+                    d4grid=d_grid(sum(n_d(1:whichisdforriskyasset(4)-1))+1:sum(n_d(1:whichisdforriskyasset(4))));
+                    d4vals=reshape(d4grid(Policy(whichisdforriskyasset(4),:,:)),[N_a*N_z,1]);
+                end
+            end
+        end
+    end
+end
+
+% Note: the relevant d for risky asset is just the
+% 'whichisdforriskyasset' d (this is typically all d)
+% riskyasset: aprime(d,u)
+if l_u==1
+    if l_drisky==1
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals, ParamCell{:});
+    elseif l_drisky==2
+        aprimeVals=arrayfun(aprimeFn, d1vals, d2vals, u1vals, ParamCell{:});
+    elseif l_drisky==3
+        aprimeVals=arrayfun(aprimeFn, d1vals, d2vals, d3vals, u1vals, ParamCell{:});
+    elseif l_drisky==4
+        aprimeVals=arrayfun(aprimeFn, d1vals, d2vals, d3vals, d4vals, u1vals, ParamCell{:});
+    end
+elseif l_u==2
+    if l_drisky==1
+        aprimeVals=arrayfun(aprimeFn, d1vals, u1vals, u2vals, ParamCell{:});
+    elseif l_drisky==2
+        aprimeVals=arrayfun(aprimeFn, d1vals, d2vals, u1vals, u2vals, ParamCell{:});
+    elseif l_drisky==3
+        aprimeVals=arrayfun(aprimeFn, d1vals, d2vals, d3vals, u1vals, u2vals, ParamCell{:});
+    elseif l_drisky==4
+        aprimeVals=arrayfun(aprimeFn, d1vals, d2vals, d3vals, d4vals, u1vals, u2vals, ParamCell{:});
+    end
+end
+
+%% Calcuate grid indexes and probs from the values
+aprimeVals=reshape(aprimeVals,[1,N_a*N_z*N_u]);
+
+riskyasset_grid=a2_grid; % hardcodes that there is only one risky asset
+
+a_griddiff=riskyasset_grid(2:end)-riskyasset_grid(1:end-1); % Distance between point and the next point
+
+temp=riskyasset_grid-aprimeVals;
+temp(temp>0)=1; % Equals 1 when a_grid is greater than aprimeVals
+
+[~,aprimeIndexes]=max(temp,[],1); % Keep the dimension corresponding to aprimeVals, minimize over the a_grid dimension
+% Note, this is going to find the 'first' grid point such that aprimeVals is smaller than or equal to that grid point
+% This is the 'upper' grid point
+
+% Switch to lower grid point index
+aprimeIndexes=aprimeIndexes-1;
+aprimeIndexes(aprimeIndexes==0)=1;
+
+% Now, find the probabilities
+aprime_residual=aprimeVals'-riskyasset_grid(aprimeIndexes);
+% Probability of the 'lower' points
+aprimeProbs=1-aprime_residual./a_griddiff(aprimeIndexes);
+
+% Those points which tried to leave the top of the grid have probability 1 of the 'upper' point (0 of lower point)
+offTopOfGrid=(aprimeVals>=riskyasset_grid(end));
+aprimeProbs(offTopOfGrid)=0;
+% Those points which tried to leave the bottom of the grid have probability 0 of the 'upper' point (1 of lower point)
+offBottomOfGrid=(aprimeVals<=riskyasset_grid(1));
+aprimeProbs(offBottomOfGrid)=1;
+
+aprimeIndexes=reshape(aprimeIndexes,[N_a,N_z,N_u]); % Index of lower grid point
+
+aprimeProbs=reshape(aprimeProbs,[N_a,N_z,N_u]); % Probability of lower grid point
+
+end
