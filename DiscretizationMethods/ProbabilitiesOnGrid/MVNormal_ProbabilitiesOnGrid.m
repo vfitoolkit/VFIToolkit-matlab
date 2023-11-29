9cf3b01efa58caaf56d45e25eab7ed6ed96e9d51
robertdkirkby <robertdkirkby@gmail.com>
2023-11-29 15:22:50 +1300

    overhaul to use mnvcdf which is much faster

--- DiscretizationMethods/ProbabilitiesOnGrid/MVNormal_ProbabilitiesOnGrid.m
+++ DiscretizationMethods/ProbabilitiesOnGrid/MVNormal_ProbabilitiesOnGrid.m
@@ -29,13 +29,15 @@ else
     end
 end
 
+bruteforce=0; % use mvncdf() [bruteforce=1 is just legacy code that acts as a double-check]
+
 if size(znum,1)>1 && size(znum,2)==1
     znum=znum'; % make it a row vector
 end
 if size(Mew,2)==1 && size(Mew,1)>1
     Mew=Mew';
     % I lie about the input size for Mew, mvncdf() actually needs to
-    % have it as a row vector, but I like it more as a column vector
+    % have it as a row vector, but toolkit likes it as a column vector
 end
 
 l_z=length(znum);
@@ -85,107 +87,89 @@ if l_z>=1
     end
 end
 
-% Preallocate for speed
-if l_z==1
-    P_part1=zeros(znum,1);
-    P_part2=zeros(znum,1);    
-else
-    P=zeros(znum);
-    P_part1=zeros(znum);
-    P_part2=zeros(znum);    
-end
+
 
 % z1_gridspacing_down
 % z1_gridspacing_up
 % z2_gridspacing_down
 % z2_gridspacing_up
 
-% Test1=1; % This is hardcoded. I leave it as something you can change so you can see the difference (with l_z=2)
-% % Now do the actual multivariate normal cdf calculation
-% if l_z==1
-%     for z1_c=1:znum(1)
-%         P_part1(z1_c)=mvncdf(z_grid(z1_c,:)+z1_gridspacing_up(z1_c),Mew,Sigma);
-%         P_part2(z1_c)=mvncdf(z_grid(z1_c,:)-z1_gridspacing_down(z1_c),Mew,Sigma);
-%     end
-%     P=P_part1-P_part2;
-% elseif l_z==2
-%     if Test1==1
-%         for z1_c=1:znum(1)
-%             for z2_c=1:znum(2)
-%                 z_c=z1_c+znum(1)*(z2_c-1);
-%                 P_part1(z1_c,z2_c)=mvncdf(z_gridvals(z_c,:)+[z1_gridspacing_up(z1_c),z2_gridspacing_up(z2_c)],Mew,Sigma);
-%                 P_part2(z1_c,z2_c)=mvncdf(z_gridvals(z_c,:)-[z1_gridspacing_down(z1_c),z2_gridspacing_down(z2_c)],Mew,Sigma);
-% 
-%                 [z1_c,z2_c,z_c]
-%                 z_gridvals(z_c,:)-[z1_gridspacing_down(z1_c),z2_gridspacing_down(z2_c)]
-%             end
-%         end
-%         P_part1
-%         P_part2
-% 
-%         P=P_part1-P_part2;
-%     elseif Test1==2
-%         for z1_c=1:znum(1)
-%             for z2_c=1:znum(2)
-%                 z_c=z1_c+znum(1)*(z2_c-1);
-%                 P_part1a(z1_c,z2_c)=mvncdf(z_gridvals(z_c,:)+[z1_gridspacing_up(z1_c),0],Mew,Sigma);
-%                 P_part2a(z1_c,z2_c)=mvncdf(z_gridvals(z_c,:)-[z1_gridspacing_down(z1_c),0],Mew,Sigma);
-%                 P_part1b(z1_c,z2_c)=mvncdf(z_gridvals(z_c,:)+[0,z2_gridspacing_up(z2_c)],Mew,Sigma);
-%                 P_part2b(z1_c,z2_c)=mvncdf(z_gridvals(z_c,:)-[0,z2_gridspacing_down(z2_c)],Mew,Sigma);
-%             end
-%         end
-%         P=(P_part1a-P_part2a)+(P_part1b-P_part2b); % This seems likely to be inferior. I just try it out as HVY2011 codes do something along these lines (taking mid grid points in each dimension seperately)
-%     end
-% end
-
-
-% Getting mvncdf() to work (unless I just ignore the correlations and use the marginals, which I don't 
-% like the vibe of) doesn't seem to work. So I just take a lazy approach. Generate a whole bunch of
-% random numbers, count them to nearest grid points, renormalize.
-nsample=10^6; % I should make this an option. Note, when I set it to 10^7 I actually got an out-of-memmory error as 'sample' was too big to fit in memory
-sample=mvnrnd(Mew,Sigma,nsample);
-
-if l_z==2
-
-    n1vec=sample(:,1);
-    temp1=(n1vec>(z1_grid'-z1_gridspacing_down')); % gives a big matrix
-    temp2=(n1vec<(z1_grid'+z1_gridspacing_up')); % gives a big matrix
-    [~,ind1]=max(temp1.*temp2,[],2);
-
-    n2vec=sample(:,2);
-    temp1=(n2vec>(z2_grid'-z2_gridspacing_down')); % gives a big matrix
-    temp2=(n2vec<(z2_grid'+z2_gridspacing_up')); % gives a big matrix
-    [~,ind2]=max(temp1.*temp2,[],2);
-
-    for ii=1:nsample
-        P(ind1(ii),ind2(ii))=P(ind1(ii),ind2(ii))+1;
+if bruteforce==0
+    %% Now do the actual multivariate normal cdf calculation
+    if l_z==1
+        z_gridspacing_up=z1_gridspacing_up;
+        z_gridspacing_down=z1_gridspacing_down;
+    elseif l_z==2
+        z_gridspacing_up=CreateGridvals(znum,[z1_gridspacing_up;z2_gridspacing_up],1);
+        z_gridspacing_down=CreateGridvals(znum,[z1_gridspacing_down;z2_gridspacing_down],1);
+    elseif l_z==3
+        z_gridspacing_up=CreateGridvals(znum,[z1_gridspacing_up;z2_gridspacing_up;z3_gridspacing_up],1);
+        z_gridspacing_down=CreateGridvals(znum,[z1_gridspacing_down;z2_gridspacing_down;z3_gridspacing_down],1);
+    elseif l_z==4
+        z_gridspacing_up=CreateGridvals(znum,[z1_gridspacing_up;z2_gridspacing_up;z3_gridspacing_up;z4_gridspacing_up],1);
+        z_gridspacing_down=CreateGridvals(znum,[z1_gridspacing_down;z2_gridspacing_down;z3_gridspacing_down;z4_gridspacing_down],1);
+    elseif l_z==5
+        z_gridspacing_up=CreateGridvals(znum,[z1_gridspacing_up;z2_gridspacing_up;z3_gridspacing_up;z4_gridspacing_up;z5_gridspacing_up],1);
+        z_gridspacing_down=CreateGridvals(znum,[z1_gridspacing_down;z2_gridspacing_down;z3_gridspacing_down;z4_gridspacing_down;z5_gridspacing_down],1);
     end
+    P=reshape(mvncdf(z_gridvals-z_gridspacing_down,z_gridvals+z_gridspacing_up,Mew,Sigma),znum);
 
-elseif l_z==3
 
-    n1vec=sample(:,1);
-    temp1=(n1vec>(z1_grid'-z1_gridspacing_down')); % gives a big matrix
-    temp2=(n1vec<(z1_grid'+z1_gridspacing_up')); % gives a big matrix
-    [~,ind1]=max(temp1.*temp2,[],2);
+else 
+    %% bruteforce=1
+    % This bruteforce is really just left here for legacy reasons (and so
+    % you can see that simulating points and then count onto grid and then
+    % normalize will give the same answer, which acts as a double-check on the
+    % above)
 
-    n2vec=sample(:,2);
-    temp1=(n2vec>(z2_grid'-z2_gridspacing_down')); % gives a big matrix
-    temp2=(n2vec<(z2_grid'+z2_gridspacing_up')); % gives a big matrix
-    [~,ind2]=max(temp1.*temp2,[],2);
+    nsample=10^6; % I should make this an option. Note, when I set it to 10^7 I actually got an out-of-memmory error as 'sample' was too big to fit in memory
+    sample=mvnrnd(Mew,Sigma,nsample);
 
-    n3vec=sample(:,3);
-    temp1=(n3vec>(z3_grid'-z3_gridspacing_down')); % gives a big matrix
-    temp2=(n3vec<(z3_grid'+z3_gridspacing_up')); % gives a big matrix
-    [~,ind3]=max(temp1.*temp2,[],2);
+    % Preallocate for speed
+    P=zeros(znum);
 
-    for ii=1:nsample
-        P(ind1(ii),ind2(ii),ind3(ii))=P(ind1(ii),ind2(ii),ind3(ii))+1;
+    if l_z==2
+
+        n1vec=sample(:,1);
+        temp1=(n1vec>(z1_grid'-z1_gridspacing_down')); % gives a big matrix
+        temp2=(n1vec<(z1_grid'+z1_gridspacing_up')); % gives a big matrix
+        [~,ind1]=max(temp1.*temp2,[],2);
+
+        n2vec=sample(:,2);
+        temp1=(n2vec>(z2_grid'-z2_gridspacing_down')); % gives a big matrix
+        temp2=(n2vec<(z2_grid'+z2_gridspacing_up')); % gives a big matrix
+        [~,ind2]=max(temp1.*temp2,[],2);
+
+        for ii=1:nsample
+            P(ind1(ii),ind2(ii))=P(ind1(ii),ind2(ii))+1;
+        end
+
+    elseif l_z==3
+
+        n1vec=sample(:,1);
+        temp1=(n1vec>(z1_grid'-z1_gridspacing_down')); % gives a big matrix
+        temp2=(n1vec<(z1_grid'+z1_gridspacing_up')); % gives a big matrix
+        [~,ind1]=max(temp1.*temp2,[],2);
+
+        n2vec=sample(:,2);
+        temp1=(n2vec>(z2_grid'-z2_gridspacing_down')); % gives a big matrix
+        temp2=(n2vec<(z2_grid'+z2_gridspacing_up')); % gives a big matrix
+        [~,ind2]=max(temp1.*temp2,[],2);
+
+        n3vec=sample(:,3);
+        temp1=(n3vec>(z3_grid'-z3_gridspacing_down')); % gives a big matrix
+        temp2=(n3vec<(z3_grid'+z3_gridspacing_up')); % gives a big matrix
+        [~,ind3]=max(temp1.*temp2,[],2);
+
+        for ii=1:nsample
+            P(ind1(ii),ind2(ii),ind3(ii))=P(ind1(ii),ind2(ii),ind3(ii))+1;
+        end
+
     end
+    % Now just normalize
+    P=P/sum(P(:));
 
 end
-% Now just normalize
-P=P/sum(P(:));
-
 
 %%
 if mvnoptions.parallel==2 
