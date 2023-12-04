83a03ff3a77daba215d234f2a6a5e147897912a9
robertdkirkby <robertdkirkby@gmail.com>
2023-12-04 15:06:00 +1300

    improve

--- Estimation/ParametrizeCoVarMatrix/GFT_inverse_mapping.m
+++ Estimation/ParametrizeCoVarMatrix/GFT_inverse_mapping.m
@@ -3,9 +3,14 @@ function [C , iter_number ] = GFT_inverse_mapping ( gamma , tol_value )
 % Archakov & Hansen (2021) - A New Parametrization of Correlation Matrices
 % Original is page 14 of https://homepage.univie.ac.at/ilya.archakov/docs/CorNoteWebAppendix.pdf
 % Explanation/illustration of how to use: https://github.com/robertdkirkby/ParametrizeCoVarianceMatrix/blob/main/CoVarMatrix.m
-
-C = [];
-iter_number = -1;
+%
+% Inputs:
+%     gamma: vector that parametrizes the correlation matrix
+%     tol_value: tolerance for the iterative algorithm that computes C from gamma
+% Outputs: 
+%     C: n-by-n correlation matrix
+%     iter_number: the number of iterations taken by the iterative algorithm
+%
 
 %% Check if input is of proper format
 % gamma is of suitable length and tolerance value belongs to a proper interval
@@ -20,29 +25,39 @@ if tol_value < 10^(-14) || tol_value  > 1^(-4)
     error('tol_value must be between 10^(-14) and 10^(-4)')
 end
 
-%% Now for the actual work :)
-% Place elements from gamma into off - diagonal parts
-% and put zeros on the main diagonal of nxn symmetric matrix A
-A = zeros(n,n);
-A(logical(tril(ones(n,n),-1))) = gamma; % Put gamma into the lower triangular elements of A (the -1 is to skip the diagonal itself)
-A = A + A'; % repeat the lower diagonal as the upper diagonal
 
-% Preallocate
-diag_vec = diag(A); % realistically, this is just preallocating as it a vector of zeros because of what we have done with A so far
-diag_ind = logical(eye(n,n));
+if n==2
+    %% n=2 has analytic solution (which will be faster)
+    % Fisher-transformation is F(p)=0.5*log((1+p)/(1-p))
+    % We just need to do inverse of this
+    p=(exp(2*gamma)-1)/(1+exp(2*gamma)); % n=1, so gamma is scalar
+    C=[1,p; p, 1];
+    iter_number=0; % no iterations
+else
+    %% Now for the actual work :)
+    % Place elements from gamma into off - diagonal parts
+    % and put zeros on the main diagonal of nxn symmetric matrix A
+    A = zeros(n,n);
+    A(logical(tril(ones(n,n),-1))) = gamma; % Put gamma into the lower triangular elements of A (the -1 is to skip the diagonal itself)
+    A = A + A'; % repeat the lower diagonal as the upper diagonal
 
-%% Iterative algorithm to get the proper diagonal vector
-dist = sqrt(n);
-while dist > sqrt(n)*tol_value
-    diag_delta = log(diag(expm(A)));
-    diag_vec = diag_vec - diag_delta ;
-    A (diag_ind) = diag_vec ;
-    dist = norm (diag_delta);
-    iter_number = iter_number + 1;
+    % Preallocate
+    diag_vec = diag(A); % realistically, this is just preallocating as it a vector of zeros because of what we have done with A so far
+    diag_ind = logical(eye(n,n));
+
+    %% Iterative algorithm to get the proper diagonal vector
+    iter_number = -1;
+    dist = sqrt(n);
+    while dist > sqrt(n)*tol_value
+        diag_delta = log(diag(expm(A)));
+        diag_vec = diag_vec - diag_delta ;
+        A (diag_ind) = diag_vec ;
+        dist = norm (diag_delta);
+        iter_number = iter_number + 1;
+    end
+    % Get a unique reciprocal correlation matrix
+    C = expm(A);
+    C(diag_ind) = ones(n,1);
 end
-% Get a unique reciprocal correlation matrix
-C = expm(A);
-C(diag_ind) = ones(n,1);
-
 
 end
