36d2e70a99ae0b8e7d1cbac8fbbf17f4a2c173f3
robertdkirkby <robertdkirkby@gmail.com>
2024-07-29 13:00:42 +1200

    eliminate Case2 and Case3 from FHorz

--- PolicyInd2Val/PolicyInd2Val_Case3.m
+++
@@ -1,7 +0,0 @@
-function PolicyValues=PolicyInd2Val_Case3(PolicyIndexes,n_d,n_a,n_z,d_grid)
-% Case2 and Case3 have same form for Policy, so just redirect it there
-% (this function is just to save user from having to know this)
-
-PolicyValues=PolicyInd2Val_Case2(PolicyIndexes,n_d,n_a,n_z,d_grid);
-
-end
