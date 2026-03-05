c3922e8475a06aa7ef33e79250c7b4e0f90b6329
Michael Tiemann <mdtiemann@gmail.com>
2026-03-05 21:49:16 +1300

    Don't try to look for age-shaped vectors when InfHorz
    Because this file handles general PType structures (even though it's name doesn't say so), we should not look for age-like shapes when `N_j_temp` is `Inf`.

--- TransitionPaths/Subcodes/PricePathParamPath_FHorz_StructToMatrix.m
+++ TransitionPaths/Subcodes/PricePathParamPath_FHorz_StructToMatrix.m
@@ -145,7 +145,7 @@ else
             ParamPathSizeVec(pp)=length(tempptypenames)*tempsize(tempsize~=T); % Get the dimension which is not T
             for ii=1:N_i
                 N_j_temp=N_j.(Names_i{ii});
-                if ~any(ParamPathSizeVec(pp)==[1,N_i,N_j_temp,N_i*N_j_temp])
+                if isfinite(N_j_temp) && ~any(ParamPathSizeVec(pp)==[1,N_i,N_j_temp,N_i*N_j_temp])
                     error(['ParamPath for ', ParamPathNames{pp}, ' appears to be the wrong size (should be 1-by-T or N_j-by-T or N_i-by-T)'])
                 end
             end
@@ -155,7 +155,7 @@ else
             ParamPathSizeVec(pp)=tempsize(tempsize~=T); % Get the dimension which is not T
             for ii=1:N_i
                 N_j_temp=N_j.(Names_i{ii});
-                if ~any(ParamPathSizeVec(pp)==[1,N_i,N_j_temp])
+                if isfinite(N_j_temp) && ~any(ParamPathSizeVec(pp)==[1,N_i,N_j_temp])
                     error(['ParamPath for ', ParamPathNames{pp}, ' appears to be the wrong size (should be 1-by-T or N_j-by-T or N_i-by-T)'])
                 end
             end
