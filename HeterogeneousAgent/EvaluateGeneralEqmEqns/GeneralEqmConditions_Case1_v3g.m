ebb787930d052ce7a076ef5005a0721bb7d4d80a
Michael Tiemann <mdtiemann@gmail.com>
2026-03-13 06:12:03 +1300

    Fix typos
    Fix some typos in comments.

--- HeterogeneousAgent/EvaluateGeneralEqmEqns/GeneralEqmConditions_Case1_v3g.m
+++ HeterogeneousAgent/EvaluateGeneralEqmEqns/GeneralEqmConditions_Case1_v3g.m
@@ -1,6 +1,6 @@
 function GeneralEqmConditionsValue=GeneralEqmConditions_Case1_v3g(GeneralEqmEqnsCell, GeneralEqmEqnParamNames, Parameters)
 % Input should be just one General Eqm Condition, and must be in the cell form (not the structure form)
-% v3g: the g indicates just one general eqm condtiion [v3 includes loop over g]
+% v3g: the g indicates just one general eqm condition [v3 includes loop over g]
 
 GeneralEqmEqnParamsVec=gpuArray(CreateVectorFromParams(Parameters,GeneralEqmEqnParamNames));
 GeneralEqmEqnParamsCell=cell(length(GeneralEqmEqnParamsVec),1);
