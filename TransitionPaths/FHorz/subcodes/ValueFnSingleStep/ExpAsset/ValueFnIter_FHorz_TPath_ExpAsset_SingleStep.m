7aa32ae101894f5178f7a34af03a6b1b9033ac9b
Michael Tiemann <mdtiemann@gmail.com>
2026-03-20 23:09:59 +1300

    Translate the ExpAsset _e codes to work without `e`
    Satisfied with the first draft of supporting models with both z and e, now implement changes to support models with z and no e.  Additional varieties can be implemented from these two as needed.

--- TransitionPaths/FHorz/subcodes/ValueFnSingleStep/ExpAsset/ValueFnIter_FHorz_TPath_ExpAsset_SingleStep.m
+++ TransitionPaths/FHorz/subcodes/ValueFnSingleStep/ExpAsset/ValueFnIter_FHorz_TPath_ExpAsset_SingleStep.m
@@ -1,20 +1,19 @@
-function [VKron, PolicyKron]=ValueFnIter_FHorz_TPath_ExpAsset_SingleStep(VKron,n_d,n_a,n_z,N_j,d_gridvals, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions)
+function [VKron, PolicyKron]=ValueFnIter_FHorz_TPath_ExpAsset_SingleStep(VKron,n_d1,n_d2,n_a1,n_a2,n_z,N_j, d_gridvals, d2_grid, a1_gridvals, a2_grid, z_gridvals_J, pi_z_J, ReturnFn, aprimeFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, aprimeFnParamNames, vfoptions)
 % The VKron input is next period value fn, the VKron output is this period.
 
-error("these are not yet the droids you are looking for")
-
-N_d=prod(n_d);
-N_a=prod(n_a);
+N_d1=prod(n_d1);
+% N_a1=prod(n_a1);
 N_z=prod(n_z);
 
 %% If get to here then not using exoticpreferences nor StateDependentVariables_z
 % N_z==0 is handled by a different command
 if vfoptions.divideandconquer==0
     if vfoptions.gridinterplayer==0
-        if N_d==0
-            [VKron,PolicyKron]=ValueFnIter_FHorz_TPath_SingleStep_nod_raw(VKron,n_a, n_z, N_j, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+        if N_d1==0
+            error("not implemented")
+            [VKron, PolicyKron]=ValueFnIter_FHorz_TPath_ExpAsset_SingleStep_nod_raw(VKron,n_a, n_z, N_j, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
         else
-            [VKron, PolicyKron]=ValueFnIter_FHorz_TPath_SingleStep_raw(VKron,n_d,n_a,n_z, N_j, d_gridvals, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+            [VKron, PolicyKron]=ValueFnIter_FHorz_TPath_ExpAsset_SingleStep_raw(VKron,n_d1,n_d2,n_a1,n_a2,n_z,N_j, d_gridvals, d2_grid, a1_gridvals, a2_grid, z_gridvals_J, pi_z_J, ReturnFn, aprimeFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, aprimeFnParamNames, vfoptions);
         end
     else
         error('Have not yet implemented grid interpolation layer for FHorz TPath without fastOLG=1. Ask on forum if you need this.')
@@ -24,7 +23,7 @@ else
 end
 
 %% Policy in transition paths
-PolicyKron=UnKronPolicyIndexes_Case1_FHorz(PolicyKron,n_d,n_a,N_z,N_j,vfoptions);
-PolicyKron=reshape(PolicyKron,[size(PolicyKron,1),N_a,N_z,N_j]);
+PolicyKron=UnKronPolicyIndexes_Case1_FHorz_ExpAsset(PolicyKron,n_d1,n_d2,n_a1,n_a2,N_z,N_j,vfoptions);
+PolicyKron=reshape(PolicyKron,[size(PolicyKron,1),prod(n_a1)*prod(n_a2),N_z,N_j]);
 
 end
