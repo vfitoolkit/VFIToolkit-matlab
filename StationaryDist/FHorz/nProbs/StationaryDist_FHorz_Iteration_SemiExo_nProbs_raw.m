7990e2f77d06fe5eed6de784f040822e3d5f4488
robertdkirkby <robertdkirkby@gmail.com>
2026-04-09 13:14:02 +1200

    new approach to iterating agent dist with semiexo

--- StationaryDist/FHorz/nProbs/StationaryDist_FHorz_Iteration_SemiExo_nProbs_raw.m
+++ StationaryDist/FHorz/nProbs/StationaryDist_FHorz_Iteration_SemiExo_nProbs_raw.m
@@ -7,24 +7,38 @@ function StationaryDist=StationaryDist_FHorz_Iteration_SemiExo_nProbs_raw(jequal
 % sparse() limits us to 2-D, and we need to get in a semiz' dimension. So I
 % put a&semiz&z together into the 1st dim, semiz'&nprobs into the 2nd dim.
 
-% Policy_aprime is currently [N_a,N_semiz*N_z,N_probs,N_j]
-Policy_aprimesemizz=repelem(reshape(gather(Policy_aprime),[N_a*N_semiz*N_z,N_probs,N_j]),1,N_semiz)+repmat(N_a*(0:1:N_semiz-1),1,N_probs)+repelem(N_a*N_semiz*(0:1:N_z-1)',N_a*N_semiz,1); % Note: add semiz' index following the semiz' dimension, add z' index following the z dimension for Tan improvement
+%%
+% It is likely that most of the elements in pi_semiz_J are zero, we can
+% take advantage of this to speed things up. Ignore for a moment the
+% dependence on d and j, and pretend it is just a N_semiz-by-N_semiz
+% matrix. Then we can calculate N_semizshort=max(sum((pi_semiz>0),2)), the
+% maximum number of non-zeros in any row of pi_semiz. And we then use this
+% in place of N_semiz as the second dimension.
+
+N_semizshort=max(max(max(sum((pi_semiz_J>0),2))));
+% Create smaller version of pi_semiz_J that eliminates as many non-zeros as possible
+[pi_semiz_J_short, idx] = sort(pi_semiz_J,2); % puts all the zeros on the left of the matrix
+
+pi_semiz_J_short=pi_semiz_J_short(:,end-N_semizshort+1:end,:,:);
+idxshort=idx(:,end-N_semizshort+1:end,:,:);
 
 Policy_dsemiexo=reshape(Policy_dsemiexo,[N_a*N_semiz*N_z,1,N_j]);
+semizindex_short=repmat(repelem((1:1:N_semiz)',N_a,1),N_z,1)+N_semiz*(0:1:N_semizshort-1)+gather((N_semiz*N_semizshort)*(Policy_dsemiexo-1))+(N_semiz*N_semizshort*N_dsemiz)*shiftdim((0:1:N_j-1),-1); % index for semiz, plus that for semiz' (in the semiz' dim) and dsemiexo; their indexes in pi_semiz_J
+pi_semiz_J_short=gather(pi_semiz_J_short);
+% semizindex_short is [N_a*N_semiz*N_z,N_semizshort,N_j]
+% used to index pi_semiz_J_short which is [N_semiz,N_semizshort,N_dsemiz,N_j]
+% and also to index the corresponding idxshort which is [N_semiz,N_semizshort,N_dsemiz,N_j]
 
-% precompute; Don't want `PolicyProbs` on GPU anyway, so leave these in CPU RAM
-semizindex=repmat(repelem((1:1:N_semiz)',N_a,1),N_z,1)+N_semiz*(0:1:N_semiz-1)+gather((N_semiz*N_semiz)*(Policy_dsemiexo-1))+(N_semiz*N_semiz*N_dsemiz)*shiftdim((0:1:N_j-1),-1); % index for semiz, plus that for semiz' (in the semiz' dim) and dsemiexo; their indexes in pi_semiz_J
-pi_semiz_J=gather(pi_semiz_J);
-% semizindex is [N_a*N_semiz*N_z,N_semiz,N_j]
-% used to index pi_semiz_J which is [N_semiz,N_semiz,N_dsemiz,N_j]
+% Policy_aprime is currently [N_a,N_semiz*N_z,N_probs,N_j]
+Policy_aprimesemizz=repelem(reshape(gather(Policy_aprime),[N_a*N_semiz*N_z,N_probs,N_j]),1,N_semizshort)+repmat(N_a*(idxshort(semizindex_short)-1),1,N_probs,1)+repelem(N_a*N_semiz*(0:1:N_z-1)',N_a*N_semiz,1); % Note: add semiz' index following the semiz' dimension, add z' index following the z dimension for Tan improvement
+% Policy_aprimesemizz is currently [N_a,N_semiz*N_z,N_probs*N_semizshort,N_j]
 
 PolicyProbs=reshape(PolicyProbs,[N_a*N_semiz*N_z,N_probs,N_j]);
-PolicyProbs=repelem(gather(PolicyProbs),1,N_semiz).*repmat(pi_semiz_J(semizindex),1,N_probs);
+PolicyProbs=repelem(gather(PolicyProbs),1,N_semizshort,1).*repmat(pi_semiz_J_short(semizindex_short),1,N_probs,1); % is of size [N_a*N_semiz*N_z,N_probs*N_semiz,N_j]
 % WHY DONT I CREATE PolicyProbs ON GPU, THEN gather()? UNLESS IT RUNS OUT OF GPU MEMORY THIS SHOULD BE FASTER?
 
 pi_z_J=gather(pi_z_J);
 
-N_bothz=N_semiz*N_z;
 
 %% Use Tan improvement
 
@@ -33,7 +47,8 @@ StationaryDist(:,1)=jequaloneDistKron;
 StationaryDist_jj=sparse(gather(jequaloneDistKron)); % use sparse matrix
 
 % Precompute; II2 used only for sparse matrix creation...best done on CPU
-II2=repelem((1:1:N_a*N_semiz*N_z)',1,N_semiz*N_probs); % Index for this period (a,semiz), note the N_probs-copies
+II2=repelem((1:1:N_a*N_semiz*N_z)',1,N_semizshort*N_probs); % Index for this period (a,semiz), note the N_probs-copies
+N_bothz=N_semiz*N_z;
 
 for jj=1:(N_j-1)
     Gammatranspose=sparse(Policy_aprimesemizz(:,:,jj),II2,PolicyProbs(:,:,jj),N_a*N_bothz,N_a*N_bothz); % Note: sparse() will accumulate at repeated indices [only relevant at grid end points]
