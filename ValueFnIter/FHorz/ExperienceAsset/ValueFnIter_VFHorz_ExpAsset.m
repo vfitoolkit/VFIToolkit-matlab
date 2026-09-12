b65781f9b9f9cd618826b612c2beafa58ecfd6fb
Michael Tiemann <mdtiemann@gmail.com>
2026-09-13 07:07:20 +1200

    WIP ExpAsset UnKron alignment

--- ValueFnIter/FHorz/ExperienceAsset/ValueFnIter_VFHorz_ExpAsset.m
+++ ValueFnIter/FHorz/ExperienceAsset/ValueFnIter_VFHorz_ExpAsset.m
@@ -56,7 +56,11 @@ for i = 1:num_a2
 end
 
 V = zeros(N_a1, N_a2, N_z_safe, N_j, 'like', a2_grid);
-PolicyKron = zeros(N_a1, N_a2, N_z_safe, N_j, 'like', a2_grid);
+if isfield(vfoptions, 'gridinterplayer') && vfoptions.gridinterplayer == 1
+    PolicyKron = zeros(3, N_a1, N_a2, N_z_safe, N_j, 'like', a2_grid);
+else
+    PolicyKron = zeros(N_a1, N_a2, N_z_safe, N_j, 'like', a2_grid);
+end
 V_next = zeros(N_a1, N_a2, N_z_safe, 'like', a2_grid);
 
 for reverse_j = 0:N_j-1
@@ -159,9 +163,16 @@ for reverse_j = 0:N_j-1
 
     d_idx = Pol_d1_max + (Pol_d2_max - 1) * N_d1_safe;
     PolicyKron_j = d_idx + (Pol_apr_max - 1) * (N_d1_safe * N_d2);
-
+    
     V(:, :, :, jj) = V_j_max;
-    PolicyKron(:, :, :, jj) = PolicyKron_j;
+    if isfield(vfoptions, 'gridinterplayer') && vfoptions.gridinterplayer == 1
+        G_segments = vfoptions.ngridinterp + 1;
+        PolicyKron(1, :, :, :, jj) = PolicyKron_j;
+        PolicyKron(2, :, :, :, jj) = G_segments + 1;
+        PolicyKron(3, :, :, :, jj) = 2;
+    else
+        PolicyKron(:, :, :, jj) = PolicyKron_j;
+    end
     V_next = V_j_max;
 end
 
@@ -171,13 +182,21 @@ PolicyKron = reshape(PolicyKron, [N_a, N_z_safe, N_j]);
 if vfoptions.outputkron == 1
     Policy = PolicyKron;
 else
-    n_d_vec = [n_d1, n_d2];
-    if N_a1 > 0 && n_a1(1) > 0
+    PolicyKron = shiftdim(PolicyKron, -1);
+    
+    if n_d1 > 0
+        n_d_vec = [n_d1, n_d2];
+    else
+        n_d_vec = n_d2;
+    end
+    
+    if n_a1 > 0 && n_a1(1) > 0
+        n_d_vec = [n_d_vec, n_a1];
         n_a_vec = [n_a1, n_a2];
     else
         n_a_vec = n_a2;
     end
-
+    
     if N_z == 0
         V = reshape(V, [N_a, N_j]);
         Policy = UnKronPolicyIndexes1_FHorz_noz(PolicyKron, n_d_vec, n_a_vec, N_j, vfoptions);
