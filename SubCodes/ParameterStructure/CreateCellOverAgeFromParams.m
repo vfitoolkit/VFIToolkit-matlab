af3142b247aca4aa1dcab32de72faf4103c2a945
Michael Tiemann <mdtiemann@gmail.com>
2026-09-17 07:24:13 +1200

    Support singlefp

--- SubCodes/ParameterStructure/CreateCellOverAgeFromParams.m
+++ SubCodes/ParameterStructure/CreateCellOverAgeFromParams.m
@@ -1,4 +1,4 @@
-function CellOverAgeOfParamValues=CreateCellOverAgeFromParams(Parameters,ParamNames,N_j,dimJ)
+function CellOverAgeOfParamValues=CreateCellOverAgeFromParams(Parameters,ParamNames,N_j,dimJ,precision)
 
 % CreateCellOverAgeFromParams looks in structure called 'Parameters' and
 % then creates a matrix with each column contains the values of it's fields that
@@ -15,13 +15,18 @@ nCalibParams=length(ParamNames);
 FullParamNames=fieldnames(Parameters);
 nFields=length(FullParamNames);
 
+if ~exist('precision')
+    % Could this be inferred from the class of Parameters?
+    precision='double';
+end
+
 % dimJ is which dimension age j should correspond to
 CellOverAgeOfParamValues=cell(1,nCalibParams);
 for iCalibParam = 1:nCalibParams
     found=0;
     for iField=1:nFields
         if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
-            CellOverAgeOfParamValues(iCalibParam)={shiftdim(reshape(gpuArray(Parameters.(FullParamNames{iField})),[length(Parameters.(FullParamNames{iField})),1]).*ones(N_j,1,'gpuArray'),1-dimJ)};
+            CellOverAgeOfParamValues(iCalibParam)={shiftdim(reshape(gpuArray(Parameters.(FullParamNames{iField})),[length(Parameters.(FullParamNames{iField})),1]).*ones(N_j,1,precision,'gpuArray'),1-dimJ)};
             % Note, if parameter depends on age this is just the column vector, if parameter does not depend on age then this turns it into a constant valued column vector
             found=1;
             break
--- SubCodes/ParameterStructure/CreateVectorFromParams.m
+++ SubCodes/ParameterStructure/CreateVectorFromParams.m
@@ -1,11 +1,14 @@
-function VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames,index1,index2)
+function CellOfParamValues=CreateCellFromParams(Parameters,ParamNames,index1,index2,precision)
 %
-% VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames)
-% VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames,index1)
-% VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames,index1,index2)
+% CellOfParamValues=CreateCellFromParams(Parameters,ParamNames)
+% CellOfParamValues=CreateCellFromParams(Parameters,ParamNames,precision)
+% CellOfParamValues=CreateCellFromParams(Parameters,ParamNames,index1)
+% CellOfParamValues=CreateCellFromParams(Parameters,ParamNames,index1,precision)
+% CellOfParamValues=CreateCellFromParams(Parameters,ParamNames,index1,index2)
+% CellOfParamValues=CreateCellFromParams(Parameters,ParamNames,index1,index2,precision)
 %
-% CreateVectorFromParams looks in structure called 'Parameters' and
-% then creates a row vector containing the values of it's fields that
+% CreateCellFromParams looks in structure called 'Parameters' and
+% then creates a cell containing the values of it's fields that
 % correspond to those field names in ParamNames (and in the order
 % given by CalibParamNames)
 %
@@ -13,72 +16,102 @@ function VectorOfParamValues=CreateVectorFromParams(Parameters,ParamNames,index1
 % matrices (eg., because the parameter values depends on age). In these
 % cases 'index1' (and 'index2') can be used to specify which is the relevant element.
 
+% 1. Intercept precision and determine the EFFECTIVE number of arguments
+nargin_temp = nargin;
+if nargin_temp == 3 && ischar(index1) && any(strcmp({'single','double'}, index1))
+    precision = index1;
+    nargin_temp = 2;
+elseif nargin_temp == 4 && ischar(index2) && any(strcmp({'single','double'}, index2))
+    precision = index2;
+    nargin_temp = 3;
+elseif nargin_temp < 5
+    precision = 'double';
+else
+    % precision was explicitly passed as the 5th argument
+    nargin_temp = 4;
+end
+
+% 2. Handle empty ParamNames cleanly
+if isempty(ParamNames)
+    CellOfParamValues = cell(1, 0);
+    return
+end
 
 nCalibParams=length(ParamNames);
 FullParamNames=fieldnames(Parameters);
 nFields=length(FullParamNames);
+CellOfParamValues=cell(1,nCalibParams);
 
-VectorOfParamValues=zeros(1,nCalibParams);
-if nargin==2
+% 3. Route to the correct block using the effective argument count
+if nargin_temp==2
     for iCalibParam = 1:nCalibParams
         found=0;
         for iField=1:nFields
             if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
-                VectorOfParamValues(iCalibParam)=gather(Parameters.(FullParamNames{iField}));
+                CellOfParamValues(iCalibParam)={cast(Parameters.(FullParamNames{iField}), precision)};
                 found=1;
                 break
             end
         end
-        if found==0 % Have added this check so that user can see if they are missing a parameter
-            warning(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
+        if found==0
+            % Have added this check so that user can see if they are missing a parameter
+            dbstack
+            error(['Failed to find parameter: ',ParamNames{iCalibParam}])
         end
     end
-elseif nargin==3
+
+elseif nargin_temp==3
     for iCalibParam = 1:nCalibParams
         found=0;
         for iField=1:nFields
             if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
-                temp=gather(Parameters.(FullParamNames{iField}));
-                if isscalar(temp) % Some parameters will depend on the index, some will not.
-                    VectorOfParamValues(iCalibParam)=temp;
+                temp=cast(gather(Parameters.(FullParamNames{iField})), precision);
+                if isscalar(temp)
+                    % Some parameters will depend on the index, some will not.
+                    CellOfParamValues(iCalibParam)={temp};
                 else
-                    VectorOfParamValues(iCalibParam)=temp(index1);
+                    CellOfParamValues(iCalibParam)={temp(index1)};
                 end
                 found=1;
                 break
             end
         end
-        if found==0 % Have added this check so that user can see if they are missing a parameter
-            warning(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
+        if found==0
+            % Have added this check so that user can see if they are missing a parameter
+            dbstack
+            error(['Failed to find parameter: ',ParamNames{iCalibParam}])
         end
     end
-elseif nargin==4
+
+elseif nargin_temp==4
     for iCalibParam = 1:nCalibParams
         found=0;
         for iField=1:nFields
             if strcmp(ParamNames{iCalibParam},FullParamNames{iField})
-                temp=gather(Parameters.(FullParamNames{iField}));
-                if isscalar(temp) % parameter is scalar, so just store it
-                    VectorOfParamValues(iCalibParam)=temp;
-                elseif numel(temp)>length(temp) % Some parameters will depend on both index1 and index2
-                    VectorOfParamValues(iCalibParam)=temp(index1,index2);
-                elseif size(temp,1)==length(temp) % Some parameters will depend only on index1.
-                    VectorOfParamValues(iCalibParam)=temp(index1);
-                elseif size(temp,2)==length(temp) % Some parameters will depend only on index2.
-                    VectorOfParamValues(iCalibParam)=temp(1,index2);
+                temp=cast(gather(Parameters.(FullParamNames{iField})), precision);
+                if isscalar(temp)
+                    % parameter is scalar, so just store it
+                    CellOfParamValues(iCalibParam)={temp};
+                elseif numel(temp)>length(temp)
+                    % Some parameters will depend on both index1 and index2
+                    CellOfParamValues(iCalibParam)={temp(index1,index2)};
+                elseif size(temp,1)==length(temp)
+                    % Some parameters will depend only on index1.
+                    CellOfParamValues(iCalibParam)={temp(index1)};
+                elseif size(temp,2)==length(temp)
+                    % Some parameters will depend only on index2.
+                    CellOfParamValues(iCalibParam)={temp(1,index2)};
                 end
                 found=1;
                 break
             end
         end
-        if found==0 % Have added this check so that user can see if they are missing a parameter
-            warning(['FAILED TO FIND PARAMETER ',ParamNames{iCalibParam}])
+        if found==0
+            % Have added this check so that user can see if they are missing a parameter
+            dbstack
+            error(['Failed to find parameter: ',ParamNames{iCalibParam}])
         end
     end
-end
 
-% if nCalibParams==0
-%     VectorOfParamValues=[]; % Have to treat this specially so that length(VectorOfParamValues) evaluates to zero
-% end
 
 end
