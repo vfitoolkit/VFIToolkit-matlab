a76f10232cb5a07286ae0d3c6e3659d4a781a77c
Michael Tiemann <mdtiemann@gmail.com>
2026-09-16 17:39:01 +1200

    Push QHEEpsteinZin else clause

--- ValueFnIter/FHorz/ValueFnIter_Case1_VFHorz.m
+++ ValueFnIter/FHorz/ValueFnIter_Case1_VFHorz.m
@@ -283,7 +283,7 @@ else
     pi_z_J = [];
 end
 
-%% --- EXOTIC PREFERENCES DISPATCH ---
+%% Quasi-Hyperbolic dispatcher (no divide-and-conquer) 
 if isfield(vfoptions, 'exoticpreferences')
     if strcmp(vfoptions.exoticpreferences, 'QuasiHyperbolic')
         if nargout == 4
@@ -291,6 +291,15 @@ if isfield(vfoptions, 'exoticpreferences')
             varargout = {V, Policy, Valt, Policyalt};
         else
             [V, Policy, Valt] = ValueFnIter_VFHorz_QuasiHyperbolic(n_d, n_a, n_z, N_j, d_grid, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+            varargout = {V, Policy, Valt, []};
+        end
+        return;
+    elseif strcmp(vfoptions.exoticpreferences, 'QHEpsteinZin')
+        if nargout == 4
+            [V, Policy, Valt, Policyalt] = ValueFnIter_VFHorz_QHEpsteinZin(n_d, n_a, n_z, N_j, d_grid, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+            varargout = {V, Policy, Valt, Policyalt};
+        else
+            [V, Policy, Valt] = ValueFnIter_VFHorz_QHEpsteinZin(n_d, n_a, n_z, N_j, d_grid, a_grid, z_gridvals_J, pi_z_J, ReturnFn, Parameters, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
             varargout = {V, Policy, Valt, []};
         end
         return;
